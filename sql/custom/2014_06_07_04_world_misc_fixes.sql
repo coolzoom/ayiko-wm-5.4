939ae28d1283b2631a8835ca1452bc9f77cb0587
Nepenthe <nepenthe@molten-wow.com>
2014-06-07 23:38:12 +0200

    [SQL] Npcs - Corrected basehealth of Pandaria creatures (refs #200)

--- sql/custom/2014_06_07_04_world_misc_fixes.sql
+++ sql/custom/2014_06_07_04_world_misc_fixes.sql
@@ -2,3 +2,6 @@
 UPDATE `creature_template` SET `faction_A` = 14, `faction_H` = 14, `npcflag` = 0, `trainer_type` = 0, `trainer_race` = 0 WHERE `entry` = 69070;
 DELETE FROM `creature` WHERE `id` = 69070 AND `map` = 0;
 DELETE FROM `npc_trainer` WHERE `entry` = 69070;
+
+-- [SQL] Corrected basehealth of Pandaria creatures refs #200
+UPDATE `creature_classlevelstats` SET `basehp4` = `basehp3` WHERE `basehp4` = 1;

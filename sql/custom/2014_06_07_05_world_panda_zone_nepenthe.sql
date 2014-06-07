364729572c3f4cbdbcc2636e613a01450ebbade7
Nepenthe <nepenthe@molten-wow.com>
2014-06-07 22:44:31 +0200

    [SQL] Quests - The Direct Solution and Do No Evil fixed (fixes #206), also 119 creatures with 1 second respawn timer corrected

--- sql/custom/2014_06_07_05_world_panda_zone_nepenthe.sql
+++ sql/custom/2014_06_07_05_world_panda_zone_nepenthe.sql
@@ -6,3 +6,9 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`
 (942035, 60566, 860, 1, 1, 41667, 0, -9061.31, 433.76, 93.0568, 0.648932, 600, 0, 0, 396, 0, 0, 3, 0, 0, 0),
 (5014527, 60566, 0, 1, 1, 0, 0, -9120.77, 393.304, 91.9913, 0.60325, 300, 0, 0, 396, 0, 0, 0, 0, 2048, 0),
 (5015055, 60567, 0, 1, 1, 0, 0, -9122.39, 394.996, 92.0043, 0.797986, 300, 0, 0, 396, 0, 0, 0, 0, 0, 0);
+
+-- [SQL] Quests - The Direct Solution and Do No Evil fixed (fixes #206), also 119 creatures with 1 second respawn timer corrected
+UPDATE `creature` SET `spawntimesecs` = 120 WHERE `spawntimesecs` = 1 AND `map` != 369;
+UPDATE `creature_template` SET `unit_flags` = 0, `dynamicflags` = 0 WHERE `entry` = 55632;
+DELETE FROM `creature_template_addon` WHERE `entry` IN (55632, 55634);
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` IN (55632, 55634));

5f28147c72f34a361c06929c7b4ae058c078525a
Nepenthe <nepenthe@molten-wow.com>
2014-06-08 00:23:10 +0200

    [SQL] [Ramires' request] Quests - Only the worthy shall pass: Firewalls phase and faction corrected so they are present and not clickable (refs# 202)

--- sql/custom/2014_06_07_04_world_misc_fixes.sql
+++ sql/custom/2014_06_07_04_world_misc_fixes.sql
@@ -3,5 +3,9 @@ UPDATE `creature_template` SET `faction_A` = 14, `faction_H` = 14, `npcflag` = 0
 DELETE FROM `creature` WHERE `id` = 69070 AND `map` = 0;
 DELETE FROM `npc_trainer` WHERE `entry` = 69070;
 
--- [SQL] Corrected basehealth of Pandaria creatures refs #200
+-- [SQL] Npcs - Corrected basehealth of Pandaria creatures (refs #200)
 UPDATE `creature_classlevelstats` SET `basehp4` = `basehp3` WHERE `basehp4` = 1;
+
+-- [SQL] [Ramires' request] Quests - Only the worthy shall pass: Firewalls phase and faction corrected so they are present and not clickable (refs# 202)
+UPDATE `gameobject_template` SET `faction` = 114 WHERE `entry` = 209375;
+UPDATE `gameobject` SET `phaseMask` = 1032 WHERE `id` = 209375;

53c3704a20e30373c8fac8aae1881519558abe0e
Nepenthe <nepenthe@molten-wow.com>
2014-06-08 04:02:16 +0200

    [SQL] [Ramires' report] Npcs - Master Shang Xi will now sit on a bench (ref #138)

--- sql/custom/2014_06_07_04_world_misc_fixes.sql
+++ sql/custom/2014_06_07_04_world_misc_fixes.sql
@@ -9,3 +9,8 @@ UPDATE `creature_classlevelstats` SET `basehp4` = `basehp3` WHERE `basehp4` = 1;
 -- [SQL] [Ramires' request] Quests - Only the worthy shall pass: Firewalls phase and faction corrected so they are present and not clickable (refs# 202)
 UPDATE `gameobject_template` SET `faction` = 114 WHERE `entry` = 209375;
 UPDATE `gameobject` SET `phaseMask` = 1032 WHERE `id` = 209375;
+
+-- [SQL] [Ramires' report] Npcs - Master Shang Xi will now sit on a bench (ref #138)
+DELETE FROM `creature_addon` WHERE `guid` = 940754;
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(940754, 0, 0, 0, 0, 0, '123161'); -- Cosmetic - Sit Chair High (Lower/Upper Body - High/Low Priority) (Anim Kit)

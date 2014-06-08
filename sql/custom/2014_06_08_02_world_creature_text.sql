5cf20515780ddd3fc23104d7fbe81efbbc6fb9e0
Raufen <Feanordev@gmail.com>
2014-06-08 21:39:59 +0200

    [SQL] (fixes #127) Text corrections

--- sql/custom/2014_06_08_02_world_creature_text.sql
+++ sql/custom/2014_06_08_02_world_creature_text.sql
@@ -1,1 +1,20 @@
 UPDATE creature_text SET text = REPLACE(text, '$N', '$n');
+
+UPDATE `creature_text` SET `text`='Scourge filth! DIE!' WHERE (`entry`='28936') AND (`groupid`='0') AND (`id`='0');
+
+UPDATE `creature_text` SET `text`='HELP! HELP! ' WHERE (`entry`='28941') AND (`groupid`='0') AND (`id`='7');
+UPDATE `creature_text` SET `text`='Die Scourge dog! DIE!' WHERE (`entry`='28941') AND (`groupid`='1') AND (`id`='1');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='28941') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='Ask for anything and you shall have it! Just spare my life!' WHERE (`entry`='28941') AND (`groupid`='0') AND (`id`='6');
+UPDATE `creature_text` SET `text`='Come then, death knight!' WHERE (`entry`='28941') AND (`groupid`='1') AND (`id`='5');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='28560') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='28576') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='28577') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='28942') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='34851') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='Spare my life! I will leave this place forever! Please!' WHERE (`entry`='28559') AND (`groupid`='0') AND (`id`='2');
+UPDATE `creature_text` SET `text`='HELP! HELP! ' WHERE (`entry`='28559') AND (`groupid`='0') AND (`id`='7');
+UPDATE `creature_text` SET `text`='HELP! HELP! ' WHERE (`entry`='28560') AND (`groupid`='0') AND (`id`='7');
+UPDATE `creature_text` SET `text`='HELP! HELP! ' WHERE (`entry`='28941') AND (`groupid`='0') AND (`id`='7');
+UPDATE `creature_text` SET `text`='HELP! HELP! ' WHERE (`entry`='28942') AND (`groupid`='0') AND (`id`='7');
+UPDATE `creature_text` SET `text`='HELP! HELP! ' WHERE (`entry`='34851') AND (`groupid`='0') AND (`id`='7');

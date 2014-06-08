cb534e80761663c9ab7dab2de239182f30eaeaf5
Raufen <Feanordev@gmail.com>
2014-06-08 22:25:29 +0200

    [SQL] (refs #128) Removed double-spawn of Knight Commander Plaguefist Corrected wrong SuggestedPlayers for new quests

--- sql/custom/2014_06_08_03_world_world_fixes.sql
+++ sql/custom/2014_06_08_03_world_world_fixes.sql
@@ -2,3 +2,7 @@ DELETE FROM `quest_start_scripts` WHERE (`id`='12842');
 UPDATE `quest_template` SET `StartScript`='0' WHERE (`Id`='12842');
 
 DELETE FROM `creature` WHERE (`guid`='129452');
+
+DELETE FROM `creature` WHERE (`guid`='921003');
+
+UPDATE quest_template SET SuggestedPlayers = 0 WHERE SuggestedPlayers > 40;

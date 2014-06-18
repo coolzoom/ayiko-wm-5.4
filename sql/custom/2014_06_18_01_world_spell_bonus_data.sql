8641778171296ea97827a5e5a5261036e4a3b3fc
Raufen <Feanordev@gmail.com>
2014-06-18 17:34:53 +0200

    [SQL] (feedback #286) Fixed Faerie Fire AP scaling + Removed double AP scaling for spells

---
+++ sql/custom/2014_06_18_01_world_spell_bonus_data.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_bonus_data` WHERE (`entry`='770');
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES ('770', '-1', '0', '0.32', '0', 'Faerie Fire (Bear Form AP scaling)');

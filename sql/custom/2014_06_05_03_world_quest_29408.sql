46b820d947717feb421925f0858faa8da19b4153
Nepenthe <nepenthe@molten-wow.com>
2014-06-18 21:23:32 +0200

    [SQL] [Internal] added some backup line so we don't lose original values

--- sql/custom/2014_06_05_03_world_quest_29408.sql
+++ sql/custom/2014_06_05_03_world_quest_29408.sql
@@ -1,4 +1,5 @@
-UPDATE `quest_template` SET `Flags`='0', `RequiredNpcOrGo1`='59591', `RequiredNpcOrGo2`='-210986', `RequiredNpcOrGoCount1`='1', `RequiredNpcOrGoCount2`='1' WHERE (`Id`='29408');
+UPDATE `quest_template` SET `Flags`='0', `RequiredNpcOrGo1`='59591', `RequiredNpcOrGo2`='-210986', `RequiredNpcOrGoCount1`='1', `RequiredNpcOrGoCount2`='1', `WDBVerified` = '1' WHERE (`Id`='29408'); -- we need quest_template_addon for this
+-- UPDATE `quest_template` SET `Flags`='65536', `RequiredNpcOrGo1`='0', `RequiredNpcOrGo2`='0', `RequiredNpcOrGoCount1`='0', `RequiredNpcOrGoCount2`='0', `WDBVerified` = '16048' WHERE (`Id`='29408'); -- sniffs backup
 
 DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId`='18') AND (`SourceGroup`='53566') AND (`SourceEntry`='114746');
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ConditionTypeOrReference`, `ConditionValue1`, `Comment`) VALUES ('18', '53566', '114746', '9', '29408', 'Raufen - Require active quest for spellclick');

27882fe84c564c24704f306bb3dcfd0c5edffcf3
Raufen <Feanordev@gmail.com>
2014-06-19 12:33:28 +0200

    corrected coords

--- sql/custom/2014_06_19_03_world_quest_morning_breeze_village.sql
+++ sql/custom/2014_06_19_03_world_quest_morning_breeze_village.sql
@@ -4,11 +4,11 @@ UPDATE `creature_template` SET `ScriptName`='mob_master_shang_xi_temple' WHERE (
 
 DELETE FROM `script_waypoint` WHERE `entry` = '55685';
 INSERT INTO `script_waypoint` (`entry`, `pointid`, `location_x`, `location_y`, `location_z`, `point_comment`) VALUES
-('55685', '0', '926', '3610', '203', 'Raufen - Morning Breeze Village'),
-('55685', '1', '947', '3620', '207', 'Raufen - Morning Breeze Village'),
-('55685', '2', '961', '3603', '211', 'Raufen - Morning Breeze Village'),
-('55685', '3', '950', '3595', '217', 'Raufen - Morning Breeze Village'),
-('55685', '4', '946', '3594', '228', 'Raufen - Morning Breeze Village'),
-('55685', '5', '942', '3580', '228', 'Raufen - Morning Breeze Village'),
-('55685', '6', '925', '3590', '228', 'Raufen - Morning Breeze Village'),
-('55685', '7', '917', '3605', '226', 'Raufen - Morning Breeze Village');
+('55685', '0', '938', '3618', '207', 'Raufen - Morning Breeze Village'),
+('55685', '1', '955', '3609', '219', 'Raufen - Morning Breeze Village'),
+('55685', '2', '951', '3593', '233', 'Raufen - Morning Breeze Village'),
+('55685', '3', '938', '3596', '244', 'Raufen - Morning Breeze Village'),
+('55685', '4', '935', '3613', '254', 'Raufen - Morning Breeze Village'),
+('55685', '5', '945', '3620', '254', 'Raufen - Morning Breeze Village'),
+('55685', '6', '960', '3605', '253', 'Raufen - Morning Breeze Village'),
+('55685', '7', '944', '3586', '253', 'Raufen - Morning Breeze Village');

c8532bef364142d2a29970b2223f362dce58c4d1
Raufen <Feanordev@gmail.com>
2014-06-20 18:05:43 +0200

    [SQL] (feedback #313) Fixed Fervor bonus for pet

---
+++ sql/custom/2014_06_20_03_spell_linked_spell.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '82726';
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('82726', '82728', 'Fervor - Apply aura to pet');

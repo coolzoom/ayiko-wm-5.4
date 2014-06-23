654fabd2112b36d071fc7c2f02d4d59ce3fc4ae1
KosmoKoz <kosmokoz@molten-wow.com>
2014-06-23 13:04:35 +0200

    typo

--- sql/custom/2014_06_23_01_world_spell_proc_event.sql
+++ sql/custom/2014_06_23_01_world_spell_proc_event.sql
@@ -1,7 +1,7 @@
 -- Combo Breaker (137384)
 DELETE FROM spell_proc_event WHERE entry = 137384;
 INSERT INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES
-(137384, 1, 53, 0x4, 0, 0, 0x10, 0x3, 0, 100, 0);
+(137384, 1, 53, 0x4, 0, 0, 0x10, 0x3, 0, 12, 0);
 
 DELETE FROM spell_script_names WHERE spell_id = 137384;
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES

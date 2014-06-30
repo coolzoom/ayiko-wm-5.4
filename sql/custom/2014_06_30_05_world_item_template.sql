b55860706cbb98b00c1f15c6bc21292f3354456b
Nepenthe <nepenthe@molten-wow.com>
2014-06-30 19:52:18 +0200

    [SQL] Items - City Satchels will now have a reputation requirement (fixes #1490)

---
+++ sql/custom/2014_06_30_05_world_item_template.sql
@@ -0,0 +1,15 @@
+-- [SQL] Items - City Satchels will now have a reputation requirement (fixes #1490)
+UPDATE `item_template` SET `RequiredReputationFaction` = 47, `RequiredReputationRank` = 6 WHERE `entry` = 67528;
+UPDATE `item_template` SET `RequiredReputationFaction` = 54, `RequiredReputationRank` = 6 WHERE `entry` = 67530;
+UPDATE `item_template` SET `RequiredReputationFaction` = 68, `RequiredReputationRank` = 6 WHERE `entry` = 67529;
+UPDATE `item_template` SET `RequiredReputationFaction` = 69, `RequiredReputationRank` = 6 WHERE `entry` = 67526;
+UPDATE `item_template` SET `RequiredReputationFaction` = 72, `RequiredReputationRank` = 6 WHERE `entry` = 67531;
+UPDATE `item_template` SET `RequiredReputationFaction` = 76, `RequiredReputationRank` = 6 WHERE `entry` = 67533;
+UPDATE `item_template` SET `RequiredReputationFaction` = 81, `RequiredReputationRank` = 6 WHERE `entry` = 67534;
+UPDATE `item_template` SET `RequiredReputationFaction` = 530, `RequiredReputationRank` = 6 WHERE `entry` = 67536;
+UPDATE `item_template` SET `RequiredReputationFaction` = 911, `RequiredReputationRank` = 6 WHERE `entry` = 67535;
+UPDATE `item_template` SET `RequiredReputationFaction` = 930, `RequiredReputationRank` = 6 WHERE `entry` = 67527;
+UPDATE `item_template` SET `RequiredReputationFaction` = 1133, `RequiredReputationRank` = 6 WHERE `entry` = 67525;
+UPDATE `item_template` SET `RequiredReputationFaction` = 1134, `RequiredReputationRank` = 6 WHERE `entry` = 67532;
+UPDATE `item_template` SET `RequiredReputationFaction` = 1353, `RequiredReputationRank` = 6 WHERE `entry` = 92071;
+UPDATE `item_template` SET `RequiredReputationFaction` = 1352, `RequiredReputationRank` = 6 WHERE `entry` = 92070;

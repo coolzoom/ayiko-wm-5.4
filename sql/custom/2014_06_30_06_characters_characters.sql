218b1e51923b85629367399c7a49d1c388c97c07
Chernyshev Viacheslav <astellar@ro.ru>
2014-06-30 23:34:53 +0400

    [SQL] Fixed transaction failure at player's save to DB.

---
+++ sql/custom/2014_06_30_06_characters_characters.sql
@@ -0,0 +1,6 @@
+alter table characters
+  modify column power1 int not null default 0,
+  modify column power2 int not null default 0,
+  modify column power3 int not null default 0,
+  modify column power4 int not null default 0,
+  modify column power5 int not null default 0;

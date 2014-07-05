b1f7efc28aeb07b2ba5161f7e13e88a14d678aea
Raufen <Feanordev@gmail.com>
2014-07-05 16:42:09 +0200

    [SQL] (feedback #3311) Fixed Glyph of Dark Archangel proccing

---
+++ sql/custom/2014_07_05_01_world_spell_proc_event.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = '58228';
+INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyName`, `SpellFamilyMask3`, `procFlags`) VALUES ('58228', '6', '16', '65536');

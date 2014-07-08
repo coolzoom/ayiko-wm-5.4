208b689daa7233e2e0bb623d8d2a81d2be17f559
Nepenthe <nepenthe@molten-wow.com>
2014-07-08 18:30:11 +0200

    [SQL] Quests - The Silkwood Road fixed (Feedback #1702)

---
+++ sql/custom/2014_07_08_02_world_creature_template.sql
@@ -0,0 +1,2 @@
+-- [SQL] Quests - The Silkwood Road fixed (Feedback #1702) 
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `faction_A` = 2030, `faction_H` = 2030 WHERE `entry` = 56070;

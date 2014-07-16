2c9b74d4f05d3d4ad47b0564fd703c6006d387f9
Nepenthe <nepenthe@molten-wow.com>
2014-07-16 19:41:00 +0200

    [SQL] Npcs - Pogeyan corrected (Fixes #560)

--- sql/custom/2014_07_16_01_quest_faces_of_evil.sql
+++ sql/custom/2014_07_16_01_quest_faces_of_evil.sql
@@ -1,5 +1,5 @@
 -- [SQL] [Cata port] Quests - Faces of Evil (A,H) scripted (Feedback #870)
-UPDATE `quest_template` SET `Method` = 2, `Flags` = 8 WHERE `Id` = 26521;
+UPDATE `quest_template` SET `Method` = 2, `Flags` = 8 WHERE `Id` IN (26521, 26357);
 UPDATE `creature` SET `modelid` = 11686 WHERE `id` = 42704;
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = 42704;
 DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 42704;
@@ -7,6 +7,6 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (42704, 0, 0, 0, 8, 0, 100, 0, 79513, 0, 0, 0, 33, 42704, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Raufen - On Spell Hit - Give KC'),
 (42704, 0, 1, 0, 8, 0, 100, 0, 79513, 0, 0, 0, 50, 181915, 180000, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Raufen - On Spell Hit - Summon Blaze (Fire)'),
 (42704, 0, 2, 0, 8, 0, 100, 0, 79513, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Raufen - On Spell Hit - Force Despawn');
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceGroup` = 1 AND `SourceEntry` = 79513;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` = 79513;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `Comment`) VALUES
 (13, 1, 79513, 31, 3, 42704, 'Raufen - Tiki Torch - Require Tiki Mask bunny');

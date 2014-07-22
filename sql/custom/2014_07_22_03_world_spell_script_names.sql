59f35e2589591e61d0d3a139f173a039156ee7e5
Raufen <Feanordev@gmail.com>
2014-07-22 20:29:01 +0200

    Removed some wrong spell-script

--- sql/custom/2014_07_22_03_world_spell_script_names.sql
+++ sql/custom/2014_07_22_03_world_spell_script_names.sql
@@ -1,2 +1,4 @@
 DELETE FROM `spell_script_names` WHERE (`spell_id`='114232') AND (`ScriptName`='spell_pal_sanctified_wrath');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('114232', 'spell_pal_sanctified_wrath');
+
+DELETE FROM `spell_script_names` WHERE (`spell_id`='31884') AND (`ScriptName`='spell_pal_sanctified_wrath');

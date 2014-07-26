bd0444521abdd2e3a4c2fde18cd054a87fe4c203
Raufen <Feanordev@gmail.com>
2014-07-26 11:05:16 +0200

    [SQL] Removed wrong script for Selfless Healer

---
+++ sql/custom/2014_07_26_01_world_spell_script_names.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE (`spell_id`='19750') AND (`ScriptName`='spell_pal_selfless_healer');
+DELETE FROM `spell_script_names` WHERE (`spell_id`='114250') AND (`ScriptName`='spell_pal_selfless_healer_aura');

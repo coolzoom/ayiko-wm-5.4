fc375bb51144cb5fb1fc7505dc33d70c27838fcc
Raufen <Feanordev@gmail.com>
2014-08-03 22:27:10 +0200

    (feedback #1956) Removed hack(which didn't work) that made targets with Asphyxiate not attackable by melee

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -8242,15 +8242,7 @@ void AuraEffect::HandleAuraStrangulate(AuraApplication const* aurApp, uint8 mode
 
     // Asphyxiate
     if (m_spellInfo->Id == 108194)
-    {
-        int32 newZ = 10;
         target->SetControlled(apply, UNIT_STATE_STUNNED);
-
-        if (apply)
-            target->UpdateHeight(target->GetPositionZ() + newZ);
-        else
-            target->UpdateHeight(target->GetPositionZ() - newZ);
-    }
 }
 
 void AuraEffect::HandleChangeSpellVisualEffect(AuraApplication const* aurApp, uint8 mode, bool apply) const

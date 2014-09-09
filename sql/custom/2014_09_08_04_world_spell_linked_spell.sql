ef973d3e7ae0616717dc9b8cae8d90af6970d37a
Raufen <Feanordev@gmail.com>
2014-09-09 20:08:03 +0200

    [SQL] (feedback #1845) Fixed Killing Spree damage bonus

---
+++ sql/custom/2014_09_08_04_world_spell_linked_spell.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '51690' AND `spell_effect` = '61851';
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES ('51690', '61851', '2', 'Killing Spree - Damage bonus aura');

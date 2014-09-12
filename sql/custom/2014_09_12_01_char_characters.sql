6ef425c0fdb2a740978c09eb6ff8610e230a21bc
Raufen <Feanordev@gmail.com>
2014-09-12 18:21:30 +0200

    [SQL] Powers should be stored as (signed) integer, prevents crash f/e at saving Druids with Lunar energy

---
+++ sql/custom/2014_09_12_01_char_characters.sql
@@ -0,0 +1,4 @@
+ALTER TABLE characters MODIFY power1 int(10) NOT NULL DEFAULT '0';
+ALTER TABLE characters MODIFY power2 int(10) NOT NULL DEFAULT '0';
+ALTER TABLE characters MODIFY power3 int(10) NOT NULL DEFAULT '0';
+ALTER TABLE characters MODIFY power4 int(10) NOT NULL DEFAULT '0';

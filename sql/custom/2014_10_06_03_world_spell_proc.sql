ae207bf34bccb3f4ecebfe8087e2f1fbf627a727
Raufen <Feanordev@gmail.com>
2014-10-06 17:34:42 +0200

    [SQL] (feedback #1949) Polymorph will now properly interrupt from some spells

---
+++ sql/custom/2014_10_06_03_world_spell_proc.sql
@@ -0,0 +1,8 @@
+DELETE FROM `spell_proc` WHERE `spellId` IN('118', '28271', '28272', '61305', '61721', '61780');
+INSERT INTO `spell_proc` (`spellId`, `charges`) VALUES
+('118', '1'),
+('28271', '1'),
+('28272', '1'),
+('61305', '1'),
+('61721', '1'),
+('61780', '1');

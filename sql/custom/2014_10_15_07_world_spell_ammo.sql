6242be419891b2d0f0719a708bf63dbaa98f8525
Chernyshev Viacheslav <astellar@ro.ru>
2014-10-15 22:16:38 +0400

    [SQL] Added spell_ammo table and data.

---
+++ sql/custom/2014_10_15_07_world_spell_ammo.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.15  Distrib 10.0.13-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: 540_world
+-- ------------------------------------------------------
+-- Server version	10.0.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `spell_ammo`
+--
+
+DROP TABLE IF EXISTS `spell_ammo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spell_ammo` (
+  `spellEntry` int(10) unsigned NOT NULL DEFAULT '0',
+  `ammoDisplayID` int(10) unsigned NOT NULL DEFAULT '0',
+  `inventoryType` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`spellEntry`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spell_ammo`
+--
+
+LOCK TABLES `spell_ammo` WRITE;
+/*!40000 ALTER TABLE `spell_ammo` DISABLE KEYS */;
+INSERT INTO `spell_ammo` VALUES (67593,2418,24),(67595,2418,24),(68559,2418,24),(70796,2414,24),(72116,2414,24);
+/*!40000 ALTER TABLE `spell_ammo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-15 22:15:36

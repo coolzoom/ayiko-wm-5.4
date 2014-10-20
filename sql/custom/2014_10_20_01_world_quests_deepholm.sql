d76d4a415dcbfd7909f6d68c6e789a42514fe3eb
Nepenthe <nepenthe@molten-wow.com>
2014-10-20 21:27:59 +0200

    [SQL] Quests - Fly Over (A,H) scripted (Feedback #1556)

--- sql/custom/2014_10_20_01_world_quests_deepholm.sql
+++ sql/custom/2014_10_20_01_world_quests_deepholm.sql
@@ -673,3 +673,16 @@ DELETE FROM `creature_template_addon` WHERE `entry` = @NPC_DORMANT_STONEBOUND_EL
 DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` = @NPC_DORMANT_STONEBOUND_ELEMENTAL;
 INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES
 (@NPC_DORMANT_STONEBOUND_ELEMENTAL, @SPELL_GENERAL_TRIGGER, 1, 0);
+
+-- [SQL] Quests - Fly Over (A,H) scripted (Feedback #1556)
+DELETE FROM `areatrigger_involvedrelation` WHERE `quest` = 27006;
+DELETE FROM `creature_questrelation` WHERE `quest` = 27008;
+INSERT INTO `creature_questrelation` (`id`, `quest`) VALUES
+(44818, 27008);
+DELETE FROM `creature_involvedrelation` WHERE `quest` = 27008;
+INSERT INTO `creature_involvedrelation` (`id`, `quest`) VALUES
+(44818, 27008);
+UPDATE `creature_template` SET `npcflag` = `npcflag`|2 WHERE `entry` = 44818;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 6216 AND `source_type` = 2;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(6216, 2, 0, 0, 46, 0, 100, 0, 6216, 0, 0, 0, 33, 44839, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Nepenthe-Areatrigger Masters' Gate - On Trigger - Quest Credit");

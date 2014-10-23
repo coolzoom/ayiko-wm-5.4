176da74b922d9844ce5f7bc050d1f57b8bd72f7c
Chernyshev Viacheslav <astellar@ro.ru>
2014-10-23 13:56:07 +0400

    [SQL] Added missing Opening spells to Blood Elf Monk.

---
+++ sql/custom/2014_10_23_01_world_playercreateinfo_spell.sql
@@ -0,0 +1,11 @@
+replace into playercreateinfo_spell (race, class, Spell, Note)
+values
+  (10, 10,  3365, 'Opening'),
+  (10, 10,  6247, 'Opening'),
+  (10, 10,  6477, 'Opening'),
+  (10, 10,  6478, 'Opening'),
+  (10, 10, 21651, 'Opening'),
+  (10, 10, 22810, 'Opening - No Text'),
+  (10, 10, 61437, 'Opening'),
+  (10, 10, 68398, 'Opening'),
+  (10, 10, 96220, 'Opening');

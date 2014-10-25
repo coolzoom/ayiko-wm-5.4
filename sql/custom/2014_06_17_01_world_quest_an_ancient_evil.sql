dc871bb1703976377fe0228c66bb2449d892798e
Nepenthe <nepenthe@molten-wow.com>
2014-10-25 04:53:34 +0200

    [SQL] Quests - An Ancient Evil: Removed default spawn of Vordraka since it's summoned (Refs #7297)

--- sql/custom/2014_06_17_01_world_quest_an_ancient_evil.sql
+++ sql/custom/2014_06_17_01_world_quest_an_ancient_evil.sql
@@ -1,5 +1,7 @@
 UPDATE `creature_template` SET `ScriptName`='npc_jojo_ironbrow' WHERE (`entry`='55940');
 UPDATE `creature_template` SET `faction_A`='16', `faction_H`='16', `ScriptName`='boss_vordraka' WHERE (`entry`='56009');
+DELETE FROM `creature` WHERE `id` = '56009';
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `unit_flags2`, `dynamicflags`) VALUES (943197, 56009, 860, 1, 65535, 0, 0, 259.592, 3938.63, 68.4739, 1.99203, 300, 0, 0, 12, 0, 0, 0, 0, 0, 0);
 
 DELETE FROM `creature_text` WHERE `entry` = '56416';
 INSERT INTO `creature_text` (`entry`, `text`, `type`, `probability`, `comment`) VALUES ('56416', 'It\'s calling allies! I\'ll keep him busy while you handle them.', '12', '100', 'Raufen - Vordraka encounter');

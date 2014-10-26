990cac9366b115800e0e5bac581b0a41bf0f4954
Raufen <Feanordev@gmail.com>
2014-10-26 12:38:42 +0100

    [SQL] (feedback #4494) Fixed quest Forge Camp: Annihilated

--- sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
+++ sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
@@ -64,3 +64,13 @@ INSERT INTO `creature_text` (`entry`, `id`, `text`, `type`, `probability`) VALUE
 ('22259', '1', 'Die!', '12', '100'),
 ('22259', '2', 'You will burn!', '12', '100'),
 ('22259', '3', 'Your end has come!', '12', '100');
+
+-- Forge Camp: Annihilated
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` IN('19067', '19210');
+
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` IN('19067', '19210');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('19067', '0', '0', '0', '8', '0', '100', '0', '33531', '0', '0', '0', '33', '19067', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC'),
+('19067', '0', '1', '0', '8', '0', '100', '0', '33531', '0', '0', '0', '37', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Die'),
+('19210', '0', '0', '0', '8', '0', '100', '0', '33532', '0', '0', '0', '33', '19210', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC'),
+('19210', '0', '1', '0', '8', '0', '100', '0', '33532', '0', '0', '0', '37', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Die');

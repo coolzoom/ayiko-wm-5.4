88eb0658559137be645cbf374390e2e53f4a49b9
Raufen <Feanordev@gmail.com>
2014-10-26 15:54:17 +0100

    [SQL] (feedback #901) Fixed quest Burn in Effigy

--- sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
+++ sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
@@ -170,3 +170,25 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 ('25308', '0', '0', '0', '8', '0', '100', '0', '45414', '0', '15000', '15000', '33', '25308', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Borean - Westrift Chasm Anomaly - On Spellhit - Quest Credit'),
 ('25310', '0', '0', '0', '8', '0', '100', '0', '45414', '0', '15000', '15000', '33', '25310', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Borean - Westrift Cleftcliff Anomaly - On Spellhit - Quest Credit'),
 ('25309', '0', '0', '0', '8', '0', '100', '0', '45414', '0', '15000', '15000', '33', '25309', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Borean - Westrift Cavern Anomaly - On Spellhit - Quest Credit');
+
+-- Burn in Effigy
+UPDATE `quest_template` SET `RequiredSpellCast4`='0' WHERE (`Id`='11656');
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` BETWEEN '25510' AND '25513';
+
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` BETWEEN '25510' AND '25513';
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('25510', '0', '0', '1', '8', '0', '100', '0', '45692', '0', '0', '0', '33', '25510', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', '1st Kvaldir Vessel (The Serpent\'s Maw) - On Spellhit \'Use Tuskarr Torch\' - Quest Credit \'Burn in Effigy\''),
+('25510', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1st Kvaldir Vessel (The Serpent\'s Maw) - On Spellhit \'Use Tuskarr Torch\' - Say'),
+('25511', '0', '0', '1', '8', '0', '100', '0', '45692', '0', '0', '0', '33', '25511', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', '2nd Kvaldir Vessel (The Kur Drakkar) - On Spellhit \'Use Tuskarr Torch\' - Quest Credit \'Burn in Effigy\''),
+('25511', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '2nd Kvaldir Vessel (The Kur Drakkar) - On Spellhit \'Use Tuskarr Torch\' - Say'),
+('25512', '0', '0', '1', '8', '0', '100', '0', '45692', '0', '0', '0', '33', '25512', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', '3rd Kvaldir Vessel (Bor\'s Hammer) - On Spellhit \'Use Tuskarr Torch\' - Quest Credit \'Burn in Effigy\''),
+('25512', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '3rd Kvaldir Vessel (Bor\'s Hammer) - On Spellhit \'Use Tuskarr Torch\' - Say'),
+('25513', '0', '0', '1', '8', '0', '100', '0', '45692', '0', '0', '0', '33', '25513', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', '4th Kvaldir Vessel (Bor\'s Anvil) - On Spellhit \'Use Tuskarr Torch\' - Quest Credit \'Burn in Effigy\''),
+('25513', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '4th Kvaldir Vessel (Bor\'s Anvil) - On Spellhit \'Use Tuskarr Torch\' - Say');
+
+DELETE FROM `creature_text` WHERE `entry` BETWEEN '25510' AND '25513';
+INSERT INTO `creature_text` (`entry`, `text`, `type`, `probability`, `comment`) VALUES
+('25510', 'The Serpent\'s Maw is engulfed in tuskarr fire!', '41', '100', '1st Kvaldir Vessel (The Serpent\'s Maw)'),
+('25511', 'The Kur Drakkar is engulfed in tuskarr fire!', '41', '100', '2nd Kvaldir Vessel (The Kur Drakkar)'),
+('25512', 'Bor\'s Hammer is engulfed in tuskarr fire!', '41', '100', '3rd Kvaldir Vessel (Bor\'s Hammer)'),
+('25513', 'Bor\'s Anvil is engulfed in tuskarr fire!', '41', '100', '4th Kvaldir Vessel (Bor\'s Anvil)');

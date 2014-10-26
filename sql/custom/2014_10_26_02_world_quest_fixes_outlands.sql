7651f4d895a52f3622a783c704fa3bf48a43fa52
Raufen <Feanordev@gmail.com>
2014-10-26 10:41:44 +0100

    [SQL] (feedback #2916) Fixed quest Blessing of Incineratus

--- sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
+++ sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
@@ -23,3 +23,15 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`='22177');
 
 DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid`  = '22177';
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('22177', '0', '0', '0', '8', '0', '100', '0', '38530', '0', '0', '0', '33', '22177', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC');
+
+-- Blessing of Incineratus
+UPDATE `quest_template` SET `RequiredSpellCast4`='0' WHERE (`Id`='9805');
+
+UPDATE `smart_scripts` SET `link`='1' WHERE (`source_type`='0') AND (`id`='0') AND (`link`='0') AND `entryorguid` IN('18110','18142','18143','18144');
+
+DELETE FROM `smart_scripts` WHERE (`source_type`='0') AND (`id`='1') AND (`link`='0') AND `entryorguid` IN('18110','18142','18143','18144');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('18110', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '33', '18110', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Windyreed Quest Credit - On Spellhit (Link) - Quest Credit'),
+('18142', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '33', '18142', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Windyreed Quest Credit - On Spellhit (Link) - Quest Credit'),
+('18143', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '33', '18143', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Windyreed Quest Credit - On Spellhit (Link) - Quest Credit'),
+('18144', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '33', '18144', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Windyreed Quest Credit - On Spellhit (Link) - Quest Credit');

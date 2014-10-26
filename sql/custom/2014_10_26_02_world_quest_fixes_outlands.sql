ca6d470d793cb491d94553bbeef373963a2f71ed
Raufen <Feanordev@gmail.com>
2014-10-26 12:59:04 +0100

    [SQL] (feedback #1886) Fixed quest Surveying the Ruins

--- sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
+++ sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
@@ -94,3 +94,12 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (1835100, 9, 3, 0, 2, 0, 100, 1, 0, 0, 0, 0, 20, 9, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lump - On Script - Stop auto-attack'),
 (1835100, 9, 4, 0, 2, 0, 100, 1, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lump - On Script - Evade'),
 (1835100, 9, 5, 0, 2, 0, 100, 1, 0, 0, 0, 0, 28, 42648, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Lump - On Script - Remove Aura Sleeping Sleep');
+
+-- Surveying the Ruins
+UPDATE `creature_template` SET `AIName`='SmartAI', `flags_extra`='130' WHERE `entry` IN('20473', '20475', '20476');
+
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` IN('20473', '20475', '20476');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('20473', '0', '0', '0', '8', '0', '100', '0', '35246', '0', '0', '0', '33', '20473', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC'),
+('20475', '0', '0', '0', '8', '0', '100', '0', '35246', '0', '0', '0', '33', '20475', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC'),
+('20476', '0', '0', '0', '8', '0', '100', '0', '35246', '0', '0', '0', '33', '20476', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC');

55b8c07519b1d3d3b9d7eb3f8761cd3ee997a5e6
Raufen <Feanordev@gmail.com>
2014-10-26 15:20:01 +0100

    [SQL] (feedback #2949, #1480) Fixed quests Monitoring the Rift: Sundered Chasm and Monitoring the Rift: Cleftcliff Anomaly

--- sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
+++ sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
@@ -161,3 +161,12 @@ UPDATE `creature_template` SET `flags_extra`='130' WHERE `entry` IN('19008', '18
 
 -- Bury Those Cockroaches!
 UPDATE `creature_template` SET `minlevel`='70', `maxlevel`='70', `faction_A`='7', `faction_H`='7', `unit_flags`='4', `flags_extra`='128' WHERE `entry` IN('25402', '25403', '25404', '25405');
+
+-- Monitoring the Rift: Sundered Chasm and Monitoring the Rift: Cleftcliff Anomaly
+UPDATE `creature_template` SET `AIName`='SmartAI', `minlevel`='60', `maxlevel`='60', `faction_A`='35', `faction_H`='35', `InhabitType`='7' WHERE `entry` IN(25308, 25310, 25309);
+
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` IN(25308, 25310, 25309);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('25308', '0', '0', '0', '8', '0', '100', '0', '45414', '0', '15000', '15000', '33', '25308', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Borean - Westrift Chasm Anomaly - On Spellhit - Quest Credit'),
+('25310', '0', '0', '0', '8', '0', '100', '0', '45414', '0', '15000', '15000', '33', '25310', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Borean - Westrift Cleftcliff Anomaly - On Spellhit - Quest Credit'),
+('25309', '0', '0', '0', '8', '0', '100', '0', '45414', '0', '15000', '15000', '33', '25309', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Borean - Westrift Cavern Anomaly - On Spellhit - Quest Credit');

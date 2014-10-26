9c0e5e413503e4f6d6d3f75b3ef75c48e9176302
Raufen <Feanordev@gmail.com>
2014-10-26 13:32:04 +0100

    [SQL] (feedback #4483) Fixed quest Administering the Salve

--- sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
+++ sql/custom/2014_10_26_02_world_quest_fixes_outlands.sql
@@ -116,3 +116,19 @@ UPDATE `smart_scripts` SET `action_param2`='30000' WHERE (`entryorguid`='1972300
 UPDATE `smart_scripts` SET `action_param2`='30000' WHERE (`entryorguid`='1972400') AND (`source_type`='9') AND (`id`='0') AND (`link`='0');
 
 UPDATE `creature` SET `spawntimesecs` = '30' WHERE `id` IN(19723, 19724);
+
+-- Administering the Salve
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`='16847');
+
+DELETE FROM `smart_scripts` WHERE `source_type`='0' AND `entryorguid` ='16847';
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('16847', '0', '0', '0', '8', '0', '100', '1', '29314', '0', '0', '0', '33', '16847', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Use Healing Salve, Gives Quest Credit'),
+('16847', '0', '0', '1', '8', '0', '100', '1', '29314', '0', '0', '0', '90', '7', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Mag\'har Grunt stands up'),
+('16847', '0', '0', '2', '8', '0', '100', '1', '29314', '0', '0', '0', '1', '1', '5000', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Say Text'),
+('16847', '0', '0', '3', '8', '0', '100', '1', '29314', '0', '0', '0', '41', '20000', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Despawn after 20 seconds');
+
+DELETE FROM `creature_text` WHERE `entry` = '16847';
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
+('16847', '1', '0', 'My strength.... is... returning!', '12', '0', '100', '0', '0', '0', 'Debilitated Mag\'har Grunt'),
+('16847', '1', '1', 'You\'ve found a cure! We will crush our enemies!', '12', '0', '100', '0', '0', '0', 'Debilitated Mag\'har Grunt'),
+('16847', '1', '2', 'You\'ve restored my health! I\'m in your debt, $N.', '12', '0', '100', '0', '0', '0', 'Debilitated Mag\'har Grunt');

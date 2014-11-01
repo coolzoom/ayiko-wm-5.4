a2342a40c0f53abcf3caa06a381942d6eb8275c4
Raufen <Feanordev@gmail.com>
2014-11-01 23:46:49 +0100

    [SQL] (feedback #1954) Fixed quests Mountain Giant Muisek and Forces of Nature: Mountain Giants

--- sql/custom/2014_11_01_04_world_quests_kalimdor.sql
+++ sql/custom/2014_11_01_04_world_quests_kalimdor.sql
@@ -9,3 +9,15 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 ('13', '7', '69228', '0', '0', '31', '0', '3', '36727', '0', '0', '0', '0', '', 'Raufen - Feed of Evil Quest'),
 ('13', '7', '69228', '0', '1', '31', '0', '3', '37155', '0', '0', '0', '0', '', 'Raufen - Feed of Evil Quest'),
 ('13', '7', '69228', '0', '2', '31', '0', '3', '37156', '0', '0', '0', '0', '', 'Raufen - Feed of Evil Quest');
+
+-- Mountain Giant Muisek and Forces of Nature: Mountain Giants
+DELETE FROM `conditions` WHERE (`SourceTypeOrReferenceId`='17') AND (`SourceGroup`='0') AND (`SourceEntry`='11889');
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+('17', '0', '11889', '0', '0', '31', '1', '3', '40026', '0', '0', '0', '0', '', 'Raufen - Require Mountain Giant'),
+('17', '0', '11889', '0', '0', '36', '1', '0', '0', '0', '1', '0', '0', '', 'Raufen - Require dead');
+
+DELETE FROM `smart_scripts` WHERE (`entryorguid`='40026') AND (`source_type`='0') AND (`link`='0') AND `id` IN('2', '3', '4');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('40026', '0', '2', '0', '8', '0', '100', '0', '80995', '0', '0', '0', '33', '43347', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Give KC'),
+('40026', '0', '3', '0', '8', '0', '100', '0', '80995', '0', '0', '0', '41', '1000', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Force Despawn'),
+('40026', '0', '4', '0', '8', '0', '100', '0', '11889', '0', '0', '0', '41', '1000', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Raufen - On Spell Hit - Force Despawn');

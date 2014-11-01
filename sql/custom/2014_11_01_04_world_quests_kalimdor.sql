9416058dfde26f395194fdf3a88e991117b4a28f
Raufen <Feanordev@gmail.com>
2014-11-01 23:16:46 +0100

    [SQL] (feedback #991) Fixed quest Challenge to the Black Flight

---
+++ sql/custom/2014_11_01_04_world_quests_kalimdor.sql
@@ -0,0 +1,3 @@
+-- Challenge to the Black Flight
+DELETE FROM `gameobject` WHERE `id` = '300153';
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation2`, `rotation3`, `spawntimesecs`, `state`) VALUES ('12009', '300153', '1', '1', '-4698.88', '-3721.16', '50.891', '3.764', '0.951929', '-0.306319', '180', '1');

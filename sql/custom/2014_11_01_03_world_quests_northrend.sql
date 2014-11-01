b2ed63cbd6461f5eb69a69502749f198e5074f6c
Raufen <Feanordev@gmail.com>
2014-11-01 19:36:56 +0100

    [SQL] (feedback #2291) Fixed quest Hampering Their Escape

--- sql/custom/2014_11_01_03_world_quests_northrend.sql
+++ sql/custom/2014_11_01_03_world_quests_northrend.sql
@@ -6,3 +6,11 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 
 DELETE FROM `creature` WHERE `id` = '26773';
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `modelid`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `curhealth`) VALUES ('47280', '26773', '571', '1', '11686', '3133.55', '2141.55', '25.032', '3.593', '30', '42');
+
+-- Hampering Their Escape
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` IN('25441', '25442', '25443');
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` IN('25441', '25442', '25443');
+INSERT INTO `smart_scripts` (`entryorguid`, `event_type`, `event_param1`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES
+('25441', '8', '45583', '33', '25441', '7', 'Raufen - On Spell Hit - Give KC'),
+('25442', '8', '45583', '33', '25442', '7', 'Raufen - On Spell Hit - Give KC'),
+('25443', '8', '45583', '33', '25443', '7', 'Raufen - On Spell Hit - Give KC');

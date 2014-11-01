7cd267f46a1b9cb4c03da8d6f01cd2b27225a233
Raufen <Feanordev@gmail.com>
2014-11-01 20:18:57 +0100

    [SQL] (feedback #5864) Fixed quest Bombard the Ballistae

--- sql/custom/2014_11_01_03_world_quests_northrend.sql
+++ sql/custom/2014_11_01_03_world_quests_northrend.sql
@@ -38,3 +38,11 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language
 (27209, 7, 1, 'THIS IS TORTURE!', 15, 0, 100, 0, 0, 0, 'Torturer LeCraft SAY_7'),
 (27209, 7, 2, 'They all... begged... for their... lives!', 15, 0, 100, 0, 0, 0, 'Torturer LeCraft SAY_7'),
 (27209, 7, 3, 'Please... stop. I\'ll do... anything....', 15, 0, 100, 0, 0, 0, 'Torturer LeCraft SAY_7');
+
+-- Bombard the Ballistae
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`='27331');
+
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` = '27331';
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+('27331', '0', '0', '1', '8', '0', '100', '0', '48345', '0', '0', '0', '33', '27331', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Bombard the Ballistae Kill Credit Bunny - On Spellhit \'Siegesmith Bombs\' - Quest Credit \'Bombard the Ballistae\''),
+('27331', '0', '1', '0', '61', '0', '100', '0', '0', '0', '0', '0', '41', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', 'Bombard the Ballistae Kill Credit Bunny - On Spellhit \'Siegesmith Bombs\' - Despawn Instant');

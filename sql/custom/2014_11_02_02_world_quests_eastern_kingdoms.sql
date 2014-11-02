dab7f39e1dfda9c524f7e56c90e2e7175abb7268
Raufen <Feanordev@gmail.com>
2014-11-02 18:58:00 +0100

    [SQL] (feedback #4484) Fixed quest Arcane Disturbances

--- sql/custom/2014_11_02_02_world_quests_eastern_kingdoms.sql
+++ sql/custom/2014_11_02_02_world_quests_eastern_kingdoms.sql
@@ -32,3 +32,15 @@ UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-44' WHERE (`entry`='
 UPDATE `npc_spellclick_spells` SET `cast_flags`='11' WHERE (`npc_entry`='42689') AND (`spell_id`='79591');
 DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` = '42689';
 UPDATE creature_template SET AIName = '' WHERE `entry` = '42689';
+
+-- Arcane Disturbances
+DELETE FROM `gameobject` WHERE `id` = '300078';
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `isActive`) VALUES
+('6000051', '300078', '0', '1', '1', '-11369.6', '-2163.95', '24.3', '0', '0', '0', '0', '1', '180', '0', '1', '0'),
+('6000052', '300078', '0', '1', '1', '-11170.3', '-1900.21', '-17.78', '0', '0', '0', '0', '1', '180', '0', '1', '0');
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` IN('18161', '18162');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN('18161', '18162') AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`, `event_type`, `event_param1`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES
+('18161', '8', '32042', '33', '18161', '7', 'Raufen - give KC on spellhit'),
+('18162', '8', '32042', '33', '18162', '7', 'Raufen - give KC on spellhit');

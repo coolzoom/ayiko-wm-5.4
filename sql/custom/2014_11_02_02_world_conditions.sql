6cb3763ee0b9e2f57b799049966f3bf4e4a18d58
Nepenthe <nepenthe@molten-wow.com>
2014-11-02 02:03:15 +0100

    Multiple changes: [SQL] Exploits - Items: Potent Murloc Pheromone targets corrected [SQL] Exploits - Items: Bag of Clams targets corrected (Refs #2270)

--- sql/custom/2014_11_02_02_world_conditions.sql
+++ sql/custom/2014_11_02_02_world_conditions.sql
@@ -3,7 +3,7 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` 
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 1, 82799, 0, 0, 31, 0, 3, 44262, 0, 0, 0, 0, '', "Potent Murloc Pheromone targets Murlocs");
 
--- [SQL] Exploits - Items: Bag of Clams targets corrected
+-- [SQL] Exploits - Items: Bag of Clams targets corrected (Refs #2270)
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceEntry` = 123803;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (17, 0, 123803, 0, 0, 31, 0, 3, 63376, 0, 0, 0, 0, '', "Bag of Clams targets Coldwhisker Otter");

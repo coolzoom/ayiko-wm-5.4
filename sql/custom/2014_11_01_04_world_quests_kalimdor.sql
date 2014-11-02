25fc71396f68c4579d5df95bbfe8306795b4287b
Raufen <Feanordev@gmail.com>
2014-11-02 22:57:46 +0100

    [SQL] (feedback #498) Fixed quest Yetiphobia

--- sql/custom/2014_11_01_04_world_quests_kalimdor.sql
+++ sql/custom/2014_11_01_04_world_quests_kalimdor.sql
@@ -202,3 +202,8 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 UPDATE `creature_template` SET `KillCredit2`='0', `AIName`='SmartAI' WHERE (`entry`='41360');
 DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` = '41360';
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('41360', '0', '0', '1', '8', '0', '100', '0', '78337', '0', '60000', '60000', '33', '41360', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Krom\'gar Marauder - KC for q26067');
+
+-- Yetiphobia
+UPDATE `creature_template` SET `faction_A`='855', `faction_H`='855', `AIName`='SmartAI' WHERE (`entry`='10978');
+DELETE FROM `smart_scripts` WHERE (`entryorguid`='10978') AND (`source_type`='0') AND (`id`='0');
+INSERT INTO `smart_scripts` (`entryorguid`, `event_type`, `event_param1`, `action_type`, `action_param1`, `target_type`, `comment`) VALUES ('10978', '8', '17166', '33', '10978', '7', 'Raufen - Give Killcredit On Spellhit');

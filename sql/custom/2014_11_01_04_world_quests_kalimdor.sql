de422d383c907a84a2f2bc525853e6823730aba0
Raufen <Feanordev@gmail.com>
2014-11-02 21:53:48 +0100

    [SQL] (feedback #1664) Fixed quest Jin'Zil's Blessing

--- sql/custom/2014_11_01_04_world_quests_kalimdor.sql
+++ sql/custom/2014_11_01_04_world_quests_kalimdor.sql
@@ -197,3 +197,8 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 ('4711', '49056', '-100', '1', '0', '1', '1'),
 ('4712', '49058', '-80', '1', '0', '1', '1'),
 ('4713', '49060', '-60', '1', '0', '1', '1');
+
+-- Jin'Zil's Blessing
+UPDATE `creature_template` SET `KillCredit2`='0', `AIName`='SmartAI' WHERE (`entry`='41360');
+DELETE FROM `smart_scripts` WHERE `source_type` = '0' AND `entryorguid` = '41360';
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('41360', '0', '0', '1', '8', '0', '100', '0', '78337', '0', '60000', '60000', '33', '41360', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', 'Nepenthe-Krom\'gar Marauder - KC for q26067');

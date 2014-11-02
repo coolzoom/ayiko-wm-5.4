73723fedfeb5c096114de6be19995c3ddf93e4a9
Raufen <Feanordev@gmail.com>
2014-11-02 17:46:29 +0100

    [SQL] (feedback #854) Increased drop-rate for The Source of the Madness quest

--- sql/custom/2014_11_02_02_world_quests_eastern_kingdoms.sql
+++ sql/custom/2014_11_02_02_world_quests_eastern_kingdoms.sql
@@ -16,3 +16,14 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `text`, `type`, `probability`) 
 ('42384', '3', 'Between you, me, and the tree, murlocs killed the Furlbrows. Yep, saw \'em with my own two eyes. Think they\'d been casin\' the joint for days, maybe months. They left in a hurry once they got wind of \"Johnny Law\" and the idiot brigade over there...', '12', '100');
 
 UPDATE `creature_template` SET `KillCredit1`='0', `KillCredit2`='0' WHERE `entry`='IN('42383', '42384', '42385', '42386', '42390', '42391', '42400', '48840');
+
+-- The Source of the Madness
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-26' WHERE (`entry`='939') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-24' WHERE (`entry`='941') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-25' WHERE (`entry`='942') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-22' WHERE (`entry`='978') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-23' WHERE (`entry`='979') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-53' WHERE (`entry`='43910') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-36' WHERE (`entry`='43911') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-33' WHERE (`entry`='43912') AND (`item`='60263');
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='-44' WHERE (`entry`='43913') AND (`item`='60263');

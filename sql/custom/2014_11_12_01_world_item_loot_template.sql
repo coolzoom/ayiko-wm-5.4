f547878d38df4d7c83688ade4fe172907e0d314e
Raufen <Feanordev@gmail.com>
2014-11-12 10:50:05 +0100

    [SQL] (feedback #9142) Fixed loot of Tailoring: Imperial Silk

---
+++ sql/custom/2014_11_12_01_world_item_loot_template.sql
@@ -0,0 +1,5 @@
+DELETE FROM `item_loot_template` WHERE `entry` = '92960';
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
+('92960', '82447', '100'),
+('92960', '90902', '10'),
+('92960', '90900', '1.5');

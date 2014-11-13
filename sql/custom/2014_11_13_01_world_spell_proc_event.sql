92dcbaf5ecf4d49739c0ffc5ca7b94202c717923
Raufen <Feanordev@gmail.com>
2014-11-13 19:15:00 +0100

    [SQL] Fixed proc of Item - Paladin PvP Set Retribution 2P Bonus

--- sql/custom/2014_11_13_01_world_spell_proc_event.sql
+++ sql/custom/2014_11_13_01_world_spell_proc_event.sql
@@ -2,3 +2,7 @@ DELETE FROM `spell_proc_event` WHERE `entry` = '131665';
 INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyName`, `SpellFamilyMask0`, `procFlags`, `CustomChance`) VALUES ('131665', '10', '1073741824', '16384', '100');
 
 UPDATE `spell_proc_event` SET `ppmRate`='0' WHERE (`entry`='31829');
+
+-- Item - Paladin PvP Set Retribution 2P Bonus
+DELETE FROM `spell_proc_event` WHERE `entry` = '131633';
+INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyName`, `SpellFamilyMask1`) VALUES ('131633', '10', '32768');

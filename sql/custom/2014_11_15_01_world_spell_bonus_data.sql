a83f01c9d8024c98ea125672f95e5666eefe657f
Raufen <Feanordev@gmail.com>
2014-11-15 21:23:16 +0100

    [SQL] (feedback #9108) Corrected Shadowburn SP scaling

--- sql/custom/2014_11_15_01_world_spell_bonus_data.sql
+++ sql/custom/2014_11_15_01_world_spell_bonus_data.sql
@@ -1,1 +1,4 @@
+-- Incinerate
 UPDATE `spell_bonus_data` SET `direct_bonus`='1.54' WHERE (`entry`='29722');
+-- Shadowburn
+UPDATE `spell_bonus_data` SET `direct_bonus`='3.5' WHERE (`entry`='17877');

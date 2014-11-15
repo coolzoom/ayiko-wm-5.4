f75536ca1a12861e638370974974f3059ed17684
Raufen <Feanordev@gmail.com>
2014-11-15 23:09:36 +0100

    [SQL] Corrected Binding Heal SP scaling

--- sql/custom/2014_11_15_01_world_spell_bonus_data.sql
+++ sql/custom/2014_11_15_01_world_spell_bonus_data.sql
@@ -4,3 +4,5 @@ UPDATE `spell_bonus_data` SET `direct_bonus`='1.54' WHERE (`entry`='29722');
 UPDATE `spell_bonus_data` SET `direct_bonus`='3.5' WHERE (`entry`='17877');
 -- Fel Flame
 UPDATE `spell_bonus_data` SET `direct_bonus`='0.85' WHERE (`entry`='77799');
+-- Binding Heal
+UPDATE `spell_bonus_data` SET `direct_bonus`='0.899' WHERE (`entry`='32546');

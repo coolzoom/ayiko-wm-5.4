13e1799f4fb04b875da614aa9bc56027a490461e
Raufen <Feanordev@gmail.com>
2014-11-15 22:22:53 +0100

    [SQL] (feedback #9109) Corrected Fel Flame SP scaling

--- sql/custom/2014_11_15_01_world_spell_bonus_data.sql
+++ sql/custom/2014_11_15_01_world_spell_bonus_data.sql
@@ -2,3 +2,5 @@
 UPDATE `spell_bonus_data` SET `direct_bonus`='1.54' WHERE (`entry`='29722');
 -- Shadowburn
 UPDATE `spell_bonus_data` SET `direct_bonus`='3.5' WHERE (`entry`='17877');
+-- Fel Flame
+UPDATE `spell_bonus_data` SET `direct_bonus`='0.85' WHERE (`entry`='77799');

2665019fcedd09ee4b37dbf2f3304324e2f0af89
Raufen <Feanordev@gmail.com>
2014-11-20 20:07:57 +0100

    [SQL] Dropped dead-code from pre 5.4

---
+++ sql/custom/2014_11_20_02_world_spell_script_names.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE (`spell_id`='108505') AND (`ScriptName`='spell_warl_archimondes_vengance');
+DELETE FROM `spell_script_names` WHERE (`spell_id`='108505') AND (`ScriptName`='spell_warl_archimondes_vengeance_cooldown');
+DELETE FROM `spell_script_names` WHERE (`spell_id`='116403') AND (`ScriptName`='spell_warl_archimondes_vengance_passive');

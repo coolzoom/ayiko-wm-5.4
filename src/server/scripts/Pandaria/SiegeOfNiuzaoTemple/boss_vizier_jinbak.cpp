17e6db5ca77e57c93500fccb31bf327bcbc2f0f7
Kosmokoz <kosmokoz@molten-wow.com>
2014-12-03 20:32:40 +0100

    [WIP][SQL]NiuzaoTemple: Added scripts for Vizier Jin'Bak and related trash

--- src/server/scripts/Pandaria/SiegeOfNiuzaoTemple/boss_vizier_jinbak.cpp
+++ src/server/scripts/Pandaria/SiegeOfNiuzaoTemple/boss_vizier_jinbak.cpp
@@ -16,39 +16,148 @@
  */
 #include "siege_of_niuzao_temple.h"
 
+enum
+{
+    SPELL_GROW                      = 119942,
+    SPELL_SAPPLING_SUMMON_VISUAL    = 120071,
+
+    NPC_GLOBULE_SUMMON_DEST         = 61629,
+    NPC_SAP_PUDDLE                  = 61613,
+    NPC_SAPPLING                    = 61623
+};
+
+typedef std::pair<float, uint64> SapplingTrigger;
+
 class boss_vizier_jinbak : public CreatureScript
 {
     enum Yells
     {
-
+        SAY_INTRO,
+        SAY_AGGRO,
+        SAY_DEATH,
+        SAY_SLAY,
+        SAY_DETONATE,
+        EMOTE_GLOBULE,
     };
 
     enum Spells
     {
-
+        SPELL_DETONATE                  = 120001,
+        SPELL_DETONATE_VISUAL           = 120095,
+        SPELL_GROW_STACKER              = 119937,
+        SPELL_SUMMON_SAPLING            = 119990
     };
 
     enum Events
     {
-
+        EVENT_DETONATE                  = 1,
+        EVENT_SUMMON_GLOBULES
     };
 
     struct boss_vizier_jinbakAI : public BossAI
     {
-        boss_vizier_jinbakAI(Creature * creature) : BossAI(creature, BOSS_JINBAK) {}
+        boss_vizier_jinbakAI(Creature * creature) : BossAI(creature, BOSS_JINBAK)
+        {
+            visualTimer = 5000;
+            puddleGUID = 0;
+            introDone = false;
+        }
 
         void Reset() override
         {
+            events.Reset();
 
+            if (Creature * trigger = Creature::GetCreature(*me, puddleGUID))
+                trigger->ToCreature()->AI()->EnterEvadeMode();
+
+            std::list<Creature *> globules;
+            me->GetCreatureListWithEntryInGrid(globules, NPC_SAPPLING, 500.0f);
+            if (!globules.empty())
+                for (auto itr : globules)
+                    itr->DespawnOrUnsummon(500);
+
+            summonDestList.clear();
+
+            visualTimer = 5000;
+        }
+
+        void MoveInLineOfSight(Unit* who)
+        {
+            if (!introDone && who->GetTypeId() == TYPEID_PLAYER)
+            {
+                if (me->IsWithinDist2d(who, 30.0f))
+                {
+                    introDone = true;
+                    Talk(SAY_INTRO);
+                }
+            }
+            BossAI::MoveInLineOfSight(who);
         }
 
         void EnterCombat(Unit* ) override
         {
+            me->InterruptNonMeleeSpells(true);
+            visualTimer = 0;
 
+            // Find nearby globule summon targets and sort by angle from centre -> npc
+            std::list<Creature *> destList;
+            me->GetCreatureListWithEntryInGrid(destList, NPC_GLOBULE_SUMMON_DEST, 500.0f);
+            Creature * sapPuddle = me->FindNearestCreature(NPC_SAP_PUDDLE, 500.0f);
+
+            ASSERT(sapPuddle != NULL);
+
+            puddleGUID = sapPuddle->GetGUID();
+
+            sapPuddle->CastSpell(sapPuddle, SPELL_GROW_STACKER, true);
+
+            for (auto itr : destList)
+            {
+                float angle = sapPuddle->GetAngle(itr);
+                summonDestList.push_back(SapplingTrigger(angle, itr->GetGUID()));
+            }
+
+            summonDestList.sort([](SapplingTrigger t1, SapplingTrigger t2) { return t1.first < t2.first; });
+
+            Talk(SAY_AGGRO);
+            events.ScheduleEvent(EVENT_SUMMON_GLOBULES, 10000);
+            events.ScheduleEvent(EVENT_DETONATE, 30000);
         }
 
-        void UpdateAI(uint32 diff) override
+        void JustSummoned(Creature* summon) override
         {
+            if (summon->GetEntry() == NPC_SAPPLING)
+            {
+                if (Creature * puddle = Creature::GetCreature(*me, puddleGUID))
+                {
+                    summon->SetReactState(REACT_PASSIVE);
+                    summon->GetMotionMaster()->MovePoint(0, *puddle);
+                }
+            }
+        }
+
+        void KilledUnit(Unit* victim) override
+        {
+            if (victim->GetTypeId() == TYPEID_PLAYER)
+                Talk(SAY_SLAY);
+        }
+
+        void JustDied(Unit* killer) override
+        {
+            Talk(SAY_DEATH);
+            _JustDied();
+        }
+
+        void UpdateAI(const uint32 diff) override
+        {
+            if (visualTimer)
+            {
+                if (visualTimer <= diff)
+                {
+                    DoCast(me, SPELL_DETONATE_VISUAL, false);
+                    visualTimer = 0;
+                } else visualTimer -= diff;
+            }
+
             if (!UpdateVictim())
                 return;
 
@@ -61,6 +170,49 @@ class boss_vizier_jinbak : public CreatureScript
             {
                 switch (eventId)
                 {
+                    case EVENT_DETONATE:
+                        Talk(SAY_DETONATE);
+                        DoCast(me, SPELL_DETONATE, false);
+                        events.ScheduleEvent(EVENT_DETONATE, 45500);
+                        break;
+                    case EVENT_SUMMON_GLOBULES:
+                    {
+                        // We need to summon them at random 3 dest NPCS
+                        // The summon position pattern looks similar to the Mercedes Logo
+                        // First, get a random starting point
+                        auto itr = summonDestList.begin();
+                        std::advance(itr, urand(0, summonDestList.size() / 3));
+                        float lastAngle = -10.0f;
+                        float minSectorAngle = 100.0f * M_PI / 180.0f;
+                        uint8 count = 3;
+
+                        for (int i = 0; i < 13; ++i)
+                        {
+                            if (itr->first < lastAngle)
+                            {
+                                // passed the 2xPI
+                                lastAngle -= M_PI * 2.0f;
+                            }
+
+                            if (fabs(itr->first - lastAngle) > minSectorAngle)
+                            {
+                                lastAngle = itr->first;
+                                if (Creature * trigger = Creature::GetCreature(*me, itr->second))
+                                    DoCast(trigger, SPELL_SUMMON_SAPLING, true);
+
+                                if (!--count)
+                                    break;
+                            }
+
+                            if (++itr == summonDestList.end())
+                                itr = summonDestList.begin();
+
+                        }
+                        Talk(EMOTE_GLOBULE);
+                        events.ScheduleEvent(EVENT_SUMMON_GLOBULES, 45500);
+
+                        break;
+                    }
                     default:
                         break;
                 }
@@ -68,6 +220,12 @@ class boss_vizier_jinbak : public CreatureScript
 
             DoMeleeAttackIfReady();
         }
+
+    private:
+        uint32 visualTimer;
+        std::list<SapplingTrigger > summonDestList;
+        uint64 puddleGUID;
+        bool introDone;
     };
 public:
     boss_vizier_jinbak() : CreatureScript("boss_vizier_jinbak") {}
@@ -78,7 +236,187 @@ public:
     }
 };
 
+
+class npc_sap_globule : public CreatureScript
+{
+    enum Spells
+    {
+        SPELL_SAP_RESIDUE           = 120070,
+
+    };
+
+    struct npc_sap_globuleAI : public ScriptedAI
+    {
+        npc_sap_globuleAI(Creature * creature) : ScriptedAI(creature) {}
+
+        void Reset() override
+        {
+
+        }
+
+        void IsSummonedBy(Unit* summoner)
+        {
+            if (Creature * jinbak = me->FindNearestCreature(NPC_JINBAK, 500.0f))
+                jinbak->AI()->JustSummoned(me);
+        }
+
+        void SpellHit(Unit * caster, SpellInfo const* spell) override
+        {
+            if (spell->Id == SPELL_SAP_RESIDUE)
+            {
+                DoCast(me, SPELL_SAPPLING_SUMMON_VISUAL, true);
+                me->DespawnOrUnsummon(100);
+            }
+        }
+
+        void UpdateAI(uint32 const diff) override
+        {
+        }
+    };
+
+public:
+    npc_sap_globule() : CreatureScript("npc_sap_globule") {}
+
+    CreatureAI * GetAI(Creature * creature) const override
+    {
+        return new npc_sap_globuleAI(creature);
+    }
+};
+
+
+class npc_sap_puddle : public CreatureScript
+{
+    enum
+    {
+        SPELL_RESIDUE_DOT           = 119941
+    };
+    struct npc_sap_puddleAI : public ScriptedAI
+    {
+        npc_sap_puddleAI(Creature * creature) : ScriptedAI(creature) {}
+
+        void Reset() override
+        {
+
+        }
+
+        void SpellHitTarget(Unit* target, SpellInfo const* spell) override
+        {
+            if (target->GetTypeId() == TYPEID_PLAYER && spell->Id == SPELL_RESIDUE_DOT)
+                if (Aura * growAura = me->GetAura(SPELL_GROW))
+                    growAura->ModStackAmount(-3); // 3 Per player each tick
+        }
+
+        void SpellHit(Unit * caster, SpellInfo const* spell) override
+        {
+            if (spell->Id == SPELL_SAPPLING_SUMMON_VISUAL)
+            {
+                Aura * growAura = me->GetAura(SPELL_GROW);
+                if (!growAura)
+                {
+                    const SpellInfo * spell = sSpellMgr->GetSpellInfo(SPELL_GROW);
+                    growAura = Aura::TryCreate(spell, MAX_EFFECT_MASK, me, me, &spell->spellPower);
+                }
+
+                if (growAura)
+                    growAura->ModStackAmount(5); // TODO: find proper value
+            }
+        }
+
+        void UpdateAI(uint32 const diff) override
+        {
+        }
+    };
+
+public:
+    npc_sap_puddle() : CreatureScript("npc_sap_puddle") {}
+
+    CreatureAI * GetAI(Creature * creature) const override
+    {
+        return new npc_sap_puddleAI(creature);
+    }
+};
+
+
+class spell_sap_residue : public SpellScriptLoader
+{
+public:
+    spell_sap_residue() : SpellScriptLoader("spell_sap_residue") {}
+
+    class spell_impl : public SpellScript
+    {
+        PrepareSpellScript(spell_impl);
+
+        void FilterTargets(std::list<WorldObject*>& targets)
+        {
+            if (Unit * caster = GetCaster())
+            {
+                //float dist = 2.0f * caster->GetAuraCount(SPELL_GROW);
+                
+                float dist = 5.0f;
+                
+                if (AuraEffect * eff = caster->GetAuraEffect(SPELL_GROW, EFFECT_0))
+                    dist *= eff->GetAmount() / 100.0f;
+
+                if (!targets.empty())
+                    targets.remove_if([&caster, &dist](const WorldObject * unit) { return unit->GetExactDist2d(caster) > dist; });
+            }
+        }
+
+        void Register()
+        {
+            OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_impl::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+            OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_impl::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENTRY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_impl();
+    }
+};
+
+// Detonate - 120001
+class spell_jinbak_detonate : public SpellScriptLoader
+{
+    enum
+    {
+        SPELL_DETONATE_DAMAGE           = 120002
+    };
+    class aura_impl : public AuraScript
+    {
+        PrepareAuraScript(aura_impl);
+
+        void OnTick(AuraEffect const *aurEff)
+        {
+            if (Unit * target = GetTarget())
+            {
+                uint32 stackAmount = std::max<uint32>(1, target->GetAuraCount(SPELL_GROW));
+                int32 damage = 8500 * stackAmount;
+                target->CastCustomSpell(target, SPELL_DETONATE_DAMAGE, &damage, NULL, NULL, true);
+                target->RemoveAurasDueToSpell(SPELL_GROW);
+            }
+        }
+
+        void Register()
+        {
+            OnEffectPeriodic += AuraEffectPeriodicFn(aura_impl::OnTick, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
+        }
+    };
+
+public:
+    spell_jinbak_detonate() : SpellScriptLoader("spell_jinbak_detonate") {}
+
+    AuraScript* GetAuraScript() const
+    {
+        return new aura_impl();
+    }
+};
+
 void AddSC_vizier_jinbak()
 {
     new boss_vizier_jinbak();
+    new npc_sap_globule();
+    new npc_sap_puddle();
+    new spell_sap_residue();
+    new spell_jinbak_detonate();
 }
--- src/server/scripts/Pandaria/SiegeOfNiuzaoTemple/siege_of_niuzao_temple.cpp
+++ src/server/scripts/Pandaria/SiegeOfNiuzaoTemple/siege_of_niuzao_temple.cpp
@@ -17,7 +17,235 @@
 
 #include "siege_of_niuzao_temple.h"
 
+/*
+***********************
+***** TRASH MOBS ******
+***********************
+*/
+
+// Sik'thik Guardian - 61928
+class npc_sikthik_guardian : public CreatureScript
+{
+    enum Spells
+    {
+        SPELL_MALLEABLE_RESIN           = 121422
+    };
+
+    struct npc_sikthik_guardianAI : public ScriptedAI
+    {
+        npc_sikthik_guardianAI(Creature * creature) : ScriptedAI(creature) {}
+
+        void Reset() override
+        {
+            chargeTimer = urand(5000, 6000);
+        }
+
+        void UpdateAI(uint32 const diff) override
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+
+            if (chargeTimer <= diff)
+            {
+                DoCast(SELECT_TARGET_RANDOM, SPELL_MALLEABLE_RESIN, true, 1);
+                chargeTimer = 10000;
+            }chargeTimer -= diff;
+
+            DoMeleeAttackIfReady();
+        }
+
+    private:
+        uint32 chargeTimer;
+    };
+
+public:
+    npc_sikthik_guardian() : CreatureScript("npc_sikthik_guardian") {}
+
+    CreatureAI * GetAI(Creature * creature) const override
+    {
+        return new npc_sikthik_guardianAI(creature);
+    }
+};
+
+
+class npc_sikthik_amber_weaver : public CreatureScript
+{
+    enum Spells
+    {
+        SPELL_RESIN_WEAVE_COSMETIC      = 120596,
+        SPELL_RESIN_WEAVING             = 121114,
+        SPELL_RESIN_SHELL               = 120946,
+    };
+
+    struct npc_sikthik_amber_weaverAI : public ArcherAI
+    {
+        npc_sikthik_amber_weaverAI(Creature * creature) : ArcherAI(creature)
+        {
+            visualTimer = 3000;
+
+
+            me->m_CombatDistance = 15.0f;
+            me->m_SightDistance = 15.0f;
+            m_minRange = 10.0f;
+        }
+
+        void Reset() override
+        {
+            resinShellTimer = urand(10000, 12000);
+            resinWeavingtimer = urand(6000, 8000);
+        }
+
+        void JustReachedHome() override
+        {
+            visualTimer = 1000;
+        }
+
+        void EnterCombat(Unit* )
+        {
+            me->InterruptNonMeleeSpells(true);
+        }
+
+        void UpdateAI(uint32 const diff) override
+        {
+            if (visualTimer)
+            {
+                if (visualTimer <= diff)
+                {
+                    DoCast(me, SPELL_RESIN_WEAVE_COSMETIC, false);
+                    visualTimer = 0;
+                } else visualTimer -= diff;
+            }
+
+            if (!UpdateVictim())
+                return;
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+
+            if (resinWeavingtimer <= diff)
+            {
+                DoCast(SELECT_TARGET_RANDOM, SPELL_RESIN_WEAVING, false);
+                resinWeavingtimer = 10000;
+                return;
+            } else resinWeavingtimer -= diff;
+
+            if (resinShellTimer <= diff)
+            {
+                DoCast(me, SPELL_RESIN_SHELL, false);
+                resinShellTimer = 20000;
+            } else resinShellTimer -= diff;
+
+            DoMeleeAttackIfReady();
+        }
+    private:
+        uint32 visualTimer;
+        uint32 resinShellTimer;
+        uint32 resinWeavingtimer;
+    };
+
+public:
+    npc_sikthik_amber_weaver() : CreatureScript("npc_sikthik_amber_weaver") {}
+
+    CreatureAI * GetAI(Creature * creature) const override
+    {
+        return new npc_sikthik_amber_weaverAI(creature);
+    }
+};
+
+
+class npc_resin_flake : public CreatureScript
+{
+    enum Spells
+    {
+        // SPELL_RESIDUE_AURA           = 120940,
+        SPELL_RESIDUE           = 120938,
+    };
+
+    struct npc_resin_flakeAI : public ScriptedAI
+    {
+        npc_resin_flakeAI(Creature * creature) : ScriptedAI(creature) {}
+
+        void Reset() override
+        {
+            residueTimer = 5000;
+        }
+
+        void UpdateAI(uint32 const diff) override
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+
+            if (residueTimer <= diff)
+            {
+                DoCast(me, SPELL_RESIDUE, true);
+                residueTimer = urand(5000, 6000);
+            } else residueTimer -= diff;
+
+            DoMeleeAttackIfReady();
+        }
+    private:
+        uint32 residueTimer;
+    };
+
+public:
+    npc_resin_flake() : CreatureScript("npc_resin_flake") {}
+
+    CreatureAI * GetAI(Creature * creature) const override
+    {
+        return new npc_resin_flakeAI(creature);
+    }
+};
+
+/*
+
+class npc_sikthik_guardian : public CreatureScript
+{
+    enum Spells
+    {
+
+    };
+
+    struct npc_sikthik_guardianAI : public ScriptedAI
+    {
+        npc_sikthik_guardianAI(Creature * creature) : ScriptedAI(creature) {}
+
+        void Reset() override
+        {
+
+        }
+
+        void UpdateAI(uint32 const diff) override
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+
+public:
+    npc_sikthik_guardian() : CreatureScript("npc_sikthik_guardian") {}
+
+    CreatureAI * GetAI(Creature * creature) const override
+    {
+        return new npc_sikthik_guardianAI(creature);
+    }
+};
+*/
+
 void AddSC_siege_of_niuzao_temple()
 {
-
+    // Hollowed Out Tree
+    new npc_sikthik_guardian();
+    new npc_sikthik_amber_weaver();
+    new npc_resin_flake();
 };

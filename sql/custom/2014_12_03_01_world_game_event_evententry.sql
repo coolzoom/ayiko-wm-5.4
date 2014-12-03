6dadd76c74cf31fe1363ac282b7e64cf11067aa1
Nepenthe <nepenthe@molten-wow.com>
2014-12-03 22:10:08 +0100

    [SQL] [Internal] Correction to commit 4098842 to make all eventEntry columns unsigned as core expects

--- sql/custom/2014_12_03_01_world_game_event_evententry.sql
+++ sql/custom/2014_12_03_01_world_game_event_evententry.sql
@@ -4,12 +4,12 @@ ALTER TABLE `game_event_battleground_holiday` MODIFY `eventEntry` MEDIUMINT(8) U
 ALTER TABLE `game_event_condition` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_creature_quest` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_gameobject_quest` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
-ALTER TABLE `game_event_model_equip` MODIFY `eventEntry` MEDIUMINT(8) NOT NULL COMMENT 'Entry of the game event.';
+ALTER TABLE `game_event_model_equip` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_npc_vendor` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_npcflag` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
-ALTER TABLE `game_event_pool` MODIFY `eventEntry` MEDIUMINT(8) NOT NULL COMMENT 'Entry of the game event.';
+ALTER TABLE `game_event_pool` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_prerequisite` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_quest_condition` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
 ALTER TABLE `game_event_seasonal_questrelation` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event.';
-ALTER TABLE `game_event_creature` MODIFY `eventEntry` MEDIUMINT(8) NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
-ALTER TABLE `game_event_gameobject` MODIFY `eventEntry` MEDIUMINT(8) NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
+ALTER TABLE `game_event_creature` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';
+ALTER TABLE `game_event_gameobject` MODIFY `eventEntry` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT 'Entry of the game event. Put negative entry to remove during event.';

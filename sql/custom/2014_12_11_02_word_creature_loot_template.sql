0e1dfd1b3c4250222d2a247b93d6d349335cf00a
Raufen <Feanordev@gmail.com>
2014-12-11 20:05:30 +0100

    [SQL] (feedback #10652) Fixed quest Eyes of Desire

---
+++ sql/custom/2014_12_11_02_word_creature_loot_template.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_loot_template` WHERE `entry` IN('19428', '20688');
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
+('19428', '72480', '-100'),
+('20688', '72480', '-100');

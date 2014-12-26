8525de6779907ad846ec7eb00b2a7a1a06872395
Raufen <Feanordev@gmail.com>
2014-12-26 15:44:09 +0100

    [SQL] Corrected Kill Command AP coeff

--- sql/custom/2014_12_26_02_world_spell_bonus_data.sql
+++ sql/custom/2014_12_26_02_world_spell_bonus_data.sql
@@ -2,3 +2,5 @@
 UPDATE `spell_bonus_data` SET `ap_bonus`='0.0382', `ap_dot_bonus`='0.0382' WHERE (`entry`='13812');
 -- Black Arrow
 UPDATE `spell_bonus_data` SET `ap_dot_bonus`='0.126' WHERE (`entry`='3674');
+-- Kill Command
+UPDATE `spell_bonus_data` SET `ap_bonus`='0.938' WHERE (`entry`='83381');

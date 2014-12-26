53a4d9c03ce663d436bb13e06f620384db017303
Raufen <Feanordev@gmail.com>
2014-12-26 15:43:44 +0100

    [SQL] Corrected Black Arrow AP coeff

--- sql/custom/2014_12_26_02_world_spell_bonus_data.sql
+++ sql/custom/2014_12_26_02_world_spell_bonus_data.sql
@@ -1,1 +1,4 @@
+-- Explosive Trap
 UPDATE `spell_bonus_data` SET `ap_bonus`='0.0382', `ap_dot_bonus`='0.0382' WHERE (`entry`='13812');
+-- Black Arrow
+UPDATE `spell_bonus_data` SET `ap_dot_bonus`='0.126' WHERE (`entry`='3674');

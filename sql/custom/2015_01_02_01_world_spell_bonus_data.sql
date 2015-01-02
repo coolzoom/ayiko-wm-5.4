09e37b804e70f40ecfbf20b06c544daf24b3a424
Raufen <Feanordev@gmail.com>
2015-01-02 17:27:21 +0100

    [SQL] (Feedback #10802) Fixed Combustion mechanic

--- sql/custom/2015_01_02_01_world_spell_bonus_data.sql
+++ sql/custom/2015_01_02_01_world_spell_bonus_data.sql
@@ -1,2 +1,6 @@
 -- Arcane Barrage
 UPDATE `spell_bonus_data` SET `direct_bonus`='1' WHERE (`entry`='44425');
+
+-- Combustion
+DELETE FROM `spell_bonus_data` WHERE `entry` = '11129';
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES ('11129', '1', '-1', '-1', '-1', 'Mage - Combustion');

031d7a162be5bd22a6047b84963f5541fc0c172e
Raufen <Feanordev@gmail.com>
2015-01-07 20:45:56 +0100

    [SQL] Blue's request, Eternal Flame HoT SP scaling correction

--- sql/custom/2015_01_07_02_world_changelog_corrections.sql
+++ sql/custom/2015_01_07_02_world_changelog_corrections.sql
@@ -1,2 +1,6 @@
 -- http://bugtracker.molten-wow.com/issues/2375
 UPDATE `creature_template` SET `faction_A`='16', `faction_H`='16', `dmg_multiplier`='10' WHERE (`entry`='58312');
+
+-- Blue's request, Eternal Flame HoT SP scaling correction
+DELETE FROM `spell_bonus_data` WHERE `entry` = '114163';
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES ('114163', '0', '-1', '-1', '-1', 'Eternal Flame - HoT Calculated in Core');

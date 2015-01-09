6f6638005dbb806aafae065137f1b5d834a6d965
Nepenthe <nepenthe@molten-wow.com>
2015-01-09 07:51:57 +0100

    [SQL] Npcs - Colossal Gyreworm fixed (Feedback #7446)

--- sql/custom/2014_10_20_01_world_quests_deepholm.sql
+++ sql/custom/2014_10_20_01_world_quests_deepholm.sql
@@ -175,6 +175,15 @@ DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` 
 -- insert into `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) values
 -- ('13','7','79715','0','0','31','0','3','42731','0','0','0','0','','Quest');
 
+-- [SQL] Npcs - Colossal Gyreworm fixed (Feedback #7446)
+SET @NPC_COLOSSAL_GYREWORM := 44258;
+SET @SPELL_CONSUMPTION := 95169;
+UPDATE `creature_template` SET `AIName` = "SmartAI", `ScriptName` = "", `unit_flags` = 0 WHERE `entry` IN (44257, 44258);
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @NPC_COLOSSAL_GYREWORM AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(@NPC_COLOSSAL_GYREWORM, 0, 0, 0, 4, 0, 100, 0, 0, 0, 0, 0, 11, @SPELL_CONSUMPTION, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Nepenthe-Colossal Gyreworm - On Aggro - Cast Consumption"),
+(@NPC_COLOSSAL_GYREWORM, 0, 1, 0, 0, 0, 100, 0, 4000, 6000, 9000, 11000, 11, @SPELL_CONSUMPTION, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Nepenthe-Colossal Gyreworm - IC - Cast Consumption");
+
 -- [SQL] Quests - Beneath the Surface scripted (Feedback #6452)
 SET @NPC_RUBY_GEMSTONE_CLUSTER := 48639;
 SET @NPC_ENORMOUS_GYREWORM := 48533;

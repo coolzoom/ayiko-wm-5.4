10ddde196b0a1f1fdddfab81b29959232ea91d14
Turk Abi <TurkAbi89@gmail.com>
2015-02-12 22:52:07 +0000

    Correction old fix

--- sql/custom/2015_02_12_world_trashmobs_immunity_and_bosses_binds.sql
+++ sql/custom/2015_02_12_world_trashmobs_immunity_and_bosses_binds.sql
@@ -1,3 +1,3 @@
-UPDATE `creature_template` SET `mechanic_immune_mask` = 653229951 WHERE `entry` = 60410;
+UPDATE `creature_template` SET `mechanic_immune_mask` = 650854271 WHERE `entry` = 60410;
 UPDATE `creature_template` SET `mechanic_immune_mask` = 8388624 WHERE `entry` IN (61131,61139,64061,64063,64068,64947);
 UPDATE `creature_template` SET `flags_extra` = 1 WHERE `entry` IN (60009,60143,60399,60400,60410);

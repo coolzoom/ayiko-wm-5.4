ff492b25be342c4b98d6467e3c616d4a528c8258
Bigpwn <p.priebusch@gmx.de>
2015-03-04 14:41:15 +0100

    Fix healing reduction effects not stacking with battle fatigue / dampening

---
+++ sql/custom/11_spell_group.sql
@@ -0,0 +1,8 @@
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '7321');
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '6136');
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '12486');
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '115804');
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '8680');
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '54680');
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES ('2021', '82654');
+INSERT INTO `spell_group_stack_rules` (`group_id`, `stack_rule`) VALUES ('2021', '3');

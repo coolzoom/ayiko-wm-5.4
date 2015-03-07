41aa64855f33bb597e44f38aa974fbd39c954d76
Bigpwn <p.priebusch@gmx.de>
2015-03-07 16:56:32 +0100

    Add forgotten sql update

--- sql/custom/world/2015_03_06_spell_proc_event.sql
+++ sql/custom/world/2015_03_06_spell_proc_event.sql
@@ -1,3 +1,6 @@
 REPLACE INTO `spell_proc_event` VALUES ('122464', '0', '0', '0', '0', '0', '0', '0', '65536', '0', '0', '10');
+
 REPLACE INTO `spell_proc_event` VALUES ('91023', '0', '8', '1792', '0', '0', '0', '0', '0', '0', '0', '0');
+
 REPLACE INTO `spell_proc_event` VALUES ('84617', '0', '8', '2', '0', '0', '0', '32', '0', '0', '20', '0');
+REPLACE INTO `spell_proc_event` VALUES ('84654', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '0');

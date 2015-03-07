24b5482e9d6d43e551ec5e4e7afe051cba23271c
Turk Abi <TurkAbi89@gmail.com>
2015-03-07 20:24:47 +0000

    [SQL] Fixed Spectral Gryphon/Whisp (Spirit Mount) for the zones where it was necessary (Added The Veiled Stair missing)

--- sql/custom/world/2015_02_28_world_ghost_zone_fix.sql
+++ sql/custom/world/2015_02_28_world_ghost_zone_fix.sql
@@ -1,5 +1,5 @@
 -- Fixed Spectral Gryphon/Whisp (Spirit Mount) for the zones where it was necessary
-DELETE FROM `spell_area` WHERE `spell` IN (55164,55173,94993) AND `area` IN (67,210,616,4815,4922,5034,5042,5144,5145,5785,5805,5840,5841,5842,6134,6138);
+DELETE FROM `spell_area` WHERE `spell` IN (55164,55173,94993) AND `area` IN (67,210,616,4815,4922,5034,5042,5144,5145,5785,5805,5840,5841,5842,6006,6134,6138);
 INSERT INTO `spell_area` VALUES 
 
 (55164,67,0,0,8326,1783,2,1,0,0),   -- (WotLK) The Storm Peaks
@@ -27,6 +27,7 @@ INSERT INTO `spell_area` VALUES
 (55164,5840,0,0,8326,1783,2,1,0,0), -- (Pandaria) Vale of Eternal Blossoms
 (55164,5841,0,0,8326,1783,2,1,0,0), -- (Pandaria) Kun-Lai Summit
 (55164,5842,0,0,8326,1783,2,1,0,0), -- (Pandaria) Towlong Steppes
+(55164,6006,0,0,8326,8,2,1,0,0),    -- (Pandaria) The Veiled Stair
 (55164,6134,0,0,8326,1783,2,1,0,0), -- (Pandaria) Krasarang Wilds
 (55164,6138,0,0,8326,1783,2,1,0,0), -- (Pandaria) Dread Wastes
 
@@ -35,5 +36,6 @@ INSERT INTO `spell_area` VALUES
 (55173,5840,0,0,8326,8,2,1,0,0),    -- (Pandaria-NightElf) Vale of Eternal Blossoms
 (55173,5841,0,0,8326,8,2,1,0,0),    -- (Pandaria-NightElf) Kun-Lai Summit
 (55173,5842,0,0,8326,8,2,1,0,0),    -- (Pandaria-NightElf) Towlong Steppes
+(55173,6006,0,0,8326,8,2,1,0,0),    -- (Pandaria-NightElf) The Veiled Stair
 (55173,6134,0,0,8326,8,2,1,0,0),    -- (Pandaria-NightElf) Krasarang Wilds
 (55173,6138,0,0,8326,8,2,1,0,0);    -- (Pandaria-NightElf) Dread Wastes

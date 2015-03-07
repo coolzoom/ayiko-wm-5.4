7f5427660fcd901e9dc074b60b650310a3100949
Bigpwn <p.priebusch@gmx.de>
2015-03-07 17:19:06 +0100

    Fix blackout kick dot being  affected by resilience, feedback #11564

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -7586,7 +7586,7 @@ void AuraEffect::HandlePeriodicDamageAurasTick(Unit* target, Unit* caster) const
         caster->CastSpell(caster, 104317, true);
 
     int32 dmg = damage;
-    if (!m_spellInfo->HasCustomAttribute(SPELL_ATTR0_CU_TRIGGERED_IGNORE_RESILENCE))
+    if (!m_spellInfo->HasCustomAttribute(SPELL_ATTR0_CU_TRIGGERED_IGNORE_RESILENCE) && !(m_spellInfo->AttributesEx4 & SPELL_ATTR4_STACK_DOT_MODIFIER))
         caster->ApplyResilience(target, &dmg);
 
     if (target != caster && target->GetTypeId() == TYPEID_PLAYER)

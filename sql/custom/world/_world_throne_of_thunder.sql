a5ef6358356d5a88c7d9acc78ad43bb6327481cf
MoltenSpark <embod92@gmail.com>
2015-03-13 17:56:11 +0100

    SQL part for Council

--- sql/custom/world/_world_throne_of_thunder.sql
+++ sql/custom/world/_world_throne_of_thunder.sql
@@ -779,5 +779,38 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry
 (13, 1, 137161, 0, 0, 31, 4, 3, 69467, 0, 0, 0, 0, '', 'Thundering Throw - Target Statue');*/
 -- Jin'rokh end
 
+-- Council of Elders
+-- UPDATE creature_template SET ScriptName = "npc_council_event_helper" WHERE entry = 662203;
+UPDATE creature_template SET ScriptName = "boss_frost_king_malakk" WHERE entry = 69131;
+UPDATE creature_template SET ScriptName = "boss_kazrajin" WHERE entry = 69134;
+UPDATE creature_template SET ScriptName = "boss_sul_the_sandcrawler" WHERE entry = 69078;
+UPDATE creature_template SET ScriptName = "boss_high_priestess_marli" WHERE entry = 69132;
+UPDATE creature_template SET ScriptName = "mob_garajals_soul" WHERE entry = 69182;
+UPDATE creature_template SET ScriptName = "mob_quicksand_stalker" WHERE entry = 662205;
+UPDATE creature_template SET ScriptName = "mob_living_sand" WHERE entry = 69153;
+UPDATE creature_template SET ScriptName = "mob_blessed_loa_spirit" WHERE entry = 69480;
+UPDATE creature_template SET ScriptName = "mob_shadowed_loa_spirit" WHERE entry = 69548;
+-- UPDATE creature_template SET ScriptName = "mob_twisted_fate_helper" WHERE entry = 662204;
+UPDATE creature_template SET ScriptName = "mob_twisted_fate" WHERE entry IN (69740, 69746);
+    
+INSERT INTO spell_script_names VALUES
+  (136442, "spell_garajal_possessed"),
+  (136903, "spell_malakk_frigid_assault"),
+  (136917, "spell_malakk_biting_cold"),
+  (136922, "spell_malakk_frostbite_periodic"),
+  (136990, "spell_malakk_frostbite"),
+  (136937, "spell_malakk_frostbite_allies"),
+  (137084, "spell_malakk_body_heat"),
+  (137117, "spell_kazrajin_reckless_charge"),
+  (137149, "spell_kazrajin_overload"),
+  (137166, "spell_kazrajin_discharge"),
+  (136860, "spell_quicksand_periodic"),
+  (136857, "spell_quicksand_entrapped"),
+  (136894, "spell_sul_sandstorm"),
+  (137203, "spell_marli_summon_blessed_loa_spirit"),
+  (137350, "spell_marli_summon_shadowed_loa_spirit"),
+  (137891, "spell_marli_twisted_fate_first"),
+  (137962, "spell_marli_twisted_fate_second"),
+  (137986, "spell_marli_twisted_fate_damages");
 
 SET FOREIGN_KEY_CHECKS = 1;

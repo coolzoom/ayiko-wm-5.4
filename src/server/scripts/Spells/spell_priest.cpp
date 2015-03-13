ae383caecd17b6c8c935843e059a33dc59732c1b
Bigpwn <p.priebusch@gmx.de>
2015-03-13 13:36:25 +0100

    Fix both surge of darkness stacks being removed with one mind spike cast

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -1383,15 +1383,7 @@ class spell_pri_mind_spike : public SpellScriptLoader
                         }
                         // ... and deals 50% additional damage.
                         else if (Aura *surgeOfDarkness = _player->GetAura(PRIEST_SURGE_OF_DARKNESS))
-                        {
                             SetHitDamage(int32(GetHitDamage() * 1.5f));
-
-                            int32 stacks = surgeOfDarkness->GetStackAmount();
-                            if (stacks <= 1)
-                                _player->RemoveAura(PRIEST_SURGE_OF_DARKNESS);
-                            else
-                                surgeOfDarkness->SetStackAmount(stacks - 1);
-                        }
                     }
                 }
             }

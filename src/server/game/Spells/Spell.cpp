59ad1adf6382ced230ba10f3ff3451713c221de9
Bigpwn <p.priebusch@gmx.de>
2015-03-18 18:54:19 +0100

    Remove artifical delay for spells that don't need it, fixes issues with soul of forest and potentially other issues. feedback #11082

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2466,28 +2466,6 @@ void Spell::AddUnitTarget(Unit* target, uint32 effectMask, bool checkIfValid /*=
             targetInfo.timeDelay = 100LL;
             m_delayMoment = 100LL;
         }
-        if (m_spellInfo->IsPositive() && (!IsTriggered() || m_spellInfo->SpellIconID == 156) && m_spellInfo->Targets != 0x40 && m_spellInfo->Id != 64382)
-        {
-            switch (m_spellInfo->Effects[0].Effect)
-            {
-                case SPELL_EFFECT_SCHOOL_DAMAGE:
-                case SPELL_EFFECT_APPLY_AURA:
-                case SPELL_EFFECT_POWER_BURN:
-                case SPELL_EFFECT_DISPEL:
-                {
-                    targetInfo.timeDelay = 100LL;
-                    m_delayMoment = 100LL;
-                }
-                default:
-                    break;
-            }
-            // Shadowstep
-            if (m_spellInfo->Id == 36563)
-            {
-                targetInfo.timeDelay = 100LL;
-                m_delayMoment = 100LL;
-            }
-        }
         else if (m_spellInfo->Id == 24259) // Spell Lock - Silenced
         {
             targetInfo.timeDelay = 100LL;

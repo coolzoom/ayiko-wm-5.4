6f1909751b531f91ad1e00d6cfe910893ea1a498
Nelidon <nelidon.dev@gmail.com>
2015-03-19 12:13:57 +0100

    [SQL] Fixed tol barad portal for Alliance (Feedback #12823)

---
+++ sql/custom/world/2015_03_19_spell_target_position.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_target_position` WHERE id=121860;
+INSERT INTO `spell_target_position` (`id`, `effIndex`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES 
+('121860', '0', '732', '-344.275', '1040.64', '21.677', '1.5651');

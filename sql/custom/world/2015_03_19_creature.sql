6ced0e8b0e9d68f9aa91d39979196f24f4d55bef
Nelidon <nelidon.dev@gmail.com>
2015-03-19 16:43:29 +0100

    [SQL] Fixed Aeonaxx (Feedback #4743)

--- sql/custom/world/2015_03_19_creature.sql
+++ sql/custom/world/2015_03_19_creature.sql
@@ -1,1 +1,5 @@
 UPDATE `creature` SET spawnMask=1 WHERE id=6491 AND map=1064;
+
+DELETE FROM `creature` WHERE id=50062;
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `npcflag2`, `unit_flags`, `unit_flags2`, `dynamicflags`, `isActive`, `protec_anti_doublet`) VALUES 
+('50062', '646', '1', '1', '0', '0', '317.643', '758.986', '261.016', '0.355575', '28800', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', NULL);

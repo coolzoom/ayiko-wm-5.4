42d9f476c3d5f9e3fef7fa7fbb7d6b70cedaf0e3
MoltenSpark <embod92@gmail.com>
2015-03-21 18:16:57 +0100

    [ToT] Add spellscript for Marked Soul (fixes Shadowed Loa Spirit)

--- sql/custom/world/_world_throne_of_thunder.sql
+++ sql/custom/world/_world_throne_of_thunder.sql
@@ -9040,7 +9040,8 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language
 (70056, 11, 0, 'We will never fail ya!', 14, 0, 100, 0, 0, 35406, 'Gara\'jal the Spiritbinder- Event12');
 
 /*---------------------------------------------------------------------------------------------------------------*/
-
+REPLACE INTO spell_script_names VALUES
+(137359, 'spell_marked_soul');
 /*---------------------------------------------------------------------------------------------------------------*/
 -- Gameobjects templates
 REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `AIName`, `ScriptName`, `WDBVerified`) VALUES

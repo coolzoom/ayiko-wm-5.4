96218c1d2fc825eb9a22ffb99496c6e0bc7b61b0
Turk Abi <TurkAbi89@gmail.com>
2015-03-31 10:56:33 +0000

    [SQL] Speaking with Xuen now give the credit (feedback #1756)

--- sql/custom/world/2015_02_17_world_creature_bug.sql
+++ sql/custom/world/2015_02_17_world_creature_bug.sql
@@ -234,3 +234,10 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (55593, 0, 0, 0, 0, 0, 50, 1, 1, 1, 0, 0, 11, 115358, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Needle Sprite - Cast Mischief"),
 (55593, 0, 1, 0, 0, 0, 100, 0, 2, 2, 6000, 7000, 11, 120770, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Needle Sprite - Cast Needles"),
 (55593, 0, 2, 0, 0, 0, 100, 0, 4000, 5000, 10000, 11000, 11, 127889, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Needle Sprite - Cast 1001 Needles");
+
+/* Speaking with Xuen now give the credit */
+UPDATE `creature_template` SET `faction_A` = 35, `faction_H` = 35, `AIName` = 'SmartAI' WHERE `entry` = 64528;
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=64528 AND `source_type`=0);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(64528, 0, 0, 1, 64, 0, 100, 0, 0, 0, 0, 0, 33, 64528, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Xuen - Give credit on gossip"),
+(64528, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Xuen - Close Gossip");

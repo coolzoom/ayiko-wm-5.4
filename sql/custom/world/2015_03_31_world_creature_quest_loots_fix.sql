cc3e8535cf782a1316532fe64c5c4972aa93b788
Turk Abi <TurkAbi89@gmail.com>
2015-04-01 15:46:58 +0000

    [SQL] Quest: Borderlands now work (feedback #2987)

--- sql/custom/world/2015_03_31_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_03_31_world_creature_quest_loots_fix.sql
@@ -67,3 +67,33 @@ INSERT INTO `creature` (`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`mo
 (60432, 870, 0, 0, 1, 1, 0, 0, -2583.11, 783.291, -1.63705, 2.25342, 120, 0, 0, 194109, 0, 0, 0, 0, 0, 2048, 0, 0),
 (60432, 870, 0, 0, 1, 1, 0, 0, -2581.55, 836.958, -29.7343, 5.18452, 120, 0, 0, 194109, 0, 0, 0, 0, 0, 2048, 0, 0),
 (60432, 870, 0, 0, 1, 1, 0, 0, -2573.17, 811.773, -63.1884, 1.63295, 120, 0, 0, 194109, 0, 0, 0, 0, 0, 2048, 0, 0);
+
+/* Quest: Borderlands fixed wrong auras,flags,templates,modelId,spawn,SAI */
+DELETE FROM `creature_template_aura` WHERE `entry` = 58065 AND `aura` = 82343;
+
+UPDATE `creature` SET `spawndist` = 0 WHERE `id` IN (58161,58170,58173);
+UPDATE `creature_template` SET `modelid1` = 11686, `modelid2` = 0, `unit_flags` = 33554432, `flags_extra` = 0, `InhabitType` = 4 WHERE `entry` IN (58065,58161,58170,58173);
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (58161,58170,58173);
+
+DELETE FROM `gameobject_template` WHERE (`entry`=210192);
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `AIName`, `ScriptName`, `WDBVerified`) VALUES 
+(210192, 8, 11114, 'Memorial Flame of Po', '', '', '', 0, 0, 3, 0, 0, 0, 0, 0, 0, 1752, 10, 0, 0, 30084, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 16048);
+
+DELETE FROM `gameobject` WHERE `id`=210192;
+INSERT INTO `gameobject` (`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`,`isActive`) VALUES
+(210192, 870, 0, 0, 1, 1, -636.533, -523.981, 70.9985, 3.67949, 0, 0, 0.964051, -0.265718, 300, 0, 1, 0);
+
+DELETE FROM `creature` WHERE `id`=58170;
+INSERT INTO `creature` (`id`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`npcflag2`,`unit_flags`,`unit_flags2`,`dynamicflags`,`isActive`) VALUES
+(58170, 870, 0, 0, 1, 1, 0, 0, -636.533, -523.981, 70.9985, 3.67949, 300, 0, 0, 156000, 0, 0, 0, 0, 0, 2048, 0, 0);
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (58161,58170,58173) AND `source_type`=0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(58161, 0, 0, 1, 8, 0, 100, 0, 110027, 0, 15000, 15000, 12, 58065, 3, 15000, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Red Beacon Bunny - Spawn Flames"),
+(58161, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 33, 58161, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Red Beacon Bunny - Give Credit"),
+
+(58170, 0, 0, 1, 8, 0, 100, 0, 110027, 0, 15000, 15000, 12, 58065, 3, 15000, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Blue Beacon Bunny - Spawn Flames"),
+(58170, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 33, 58170, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Blue Beacon Bunny - Give Credit"),
+
+(58173, 0, 0, 1, 8, 0, 100, 0, 110027, 0, 15000, 15000, 12, 58065, 3, 15000, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Green Beacon Bunny - Spawn Flames"),
+(58173, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 33, 58173, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Green Beacon Bunny - Give Credit");

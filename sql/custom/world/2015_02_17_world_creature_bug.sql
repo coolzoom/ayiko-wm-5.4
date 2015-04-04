83c2e86d01623f193fb245c9b5265b24479d1e23
Turk Abi <TurkAbi89@gmail.com>
2015-04-04 10:58:36 +0000

    [SQL] Battat correction (feedback 1764)

--- sql/custom/world/2015_02_17_world_creature_bug.sql
+++ sql/custom/world/2015_02_17_world_creature_bug.sql
@@ -224,7 +224,7 @@ DELETE FROM `smart_scripts` WHERE (`entryorguid`=60728 AND `source_type`=0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (60728, 0, 0, 0, 0, 0, 80, 0, 7000, 9000, 11000, 12000, 11, 123851, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Battat - Cast Crushing Charge"),
 (60728, 0, 1, 0, 2, 0, 100, 1, 0, 60, 0, 0, 11, 130388, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Battat - Cast Empowering Flames at 60% hp"),
-(60728, 0, 2, 0, 0, 0, 30, 0, 10000, 12000, 10000, 12000, 11, 36207, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Battat - Cast Steal Weapon");
+(60728, 0, 2, 0, 0, 0, 100, 0, 10000, 12000, 20000, 21000, 11, 36207, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Battat - Cast Steal Weapon");
 
 /* Needle Sprite wrong aura and missing SAI fixed */
 DELETE FROM `creature_template_aura` WHERE `entry` = 55593;

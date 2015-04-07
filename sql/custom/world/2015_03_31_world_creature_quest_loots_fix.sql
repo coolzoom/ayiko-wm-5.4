39285c19b87f3393dc66ccaa8bc3e572adc45c6f
Turk Abi <TurkAbi89@gmail.com>
2015-04-07 16:24:19 +0000

    [SQL] Correction old fix

--- sql/custom/world/2015_03_31_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_03_31_world_creature_quest_loots_fix.sql
@@ -1657,6 +1657,10 @@ UPDATE `creature_template` SET `exp` = 3, `Health_mod` = 15.245 WHERE `entry` = 
 
 /* Huntress Ravenoak fixed */
 UPDATE `creature_template` SET `minlevel` = 90, `maxlevel` = 90, `exp` = 4 WHERE `entry` = 14379;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=14379 AND `source_type`= 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(14379, 0, 0, 0, 9, 0, 100, 0, 4, 30, 3000, 5000, 11, 14030, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Huntress Ravenoak - Cast Hooked Net"),
+(14379, 0, 1, 0, 0, 0, 100, 0, 5000, 8000, 16000, 18000, 11, 15572, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Huntress Ravenoak - Cast Sunder Armor");
 
 /* Deer fixed */
 UPDATE `creature_template` SET `minlevel` = 1, `maxlevel` = 1, `flags_extra` = 64 WHERE `entry` = 883;

538c2330736982f8baa41dcbda8669c8a75bb9a6
Bigpwn <p.priebusch@gmx.de>
2015-04-08 00:31:11 +0200

    Fixup

--- sql/custom/world/2015_04_08_spell_script_names.sql
+++ sql/custom/world/2015_04_08_spell_script_names.sql
@@ -1,5 +1,2 @@
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('116858', 'spell_warl_ember_consumers');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('17877', 'spell_warl_ember_consumers');
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('114654', 'spell_warl_ember_consumers');
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('108685', 'spell_warl_ember_consumers');
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('108686', 'spell_warl_ember_consumers');

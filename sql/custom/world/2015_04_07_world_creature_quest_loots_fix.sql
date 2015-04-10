1a7cf4fbb778fc7d8e207af49546af121b559260
Turk Abi <TurkAbi89@gmail.com>
2015-04-10 10:15:05 +0000

    [SQL] Dire Condor fixed (feedback #606)

--- sql/custom/world/2015_04_07_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_07_world_creature_quest_loots_fix.sql
@@ -1001,3 +1001,6 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 3939;
 DELETE FROM `smart_scripts` WHERE (`entryorguid`=3939 AND `source_type`=0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (3939, 0, 0, 0, 0, 0, 100, 1, 30000, 30000, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Razormane Wolf - Despawn after 30 seconds");
+
+/* Dire Condor fixed */
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE `entry` = 428;

e7005945d776760f40df066963aba76873dc0fac
Turk Abi <TurkAbi89@gmail.com>
2015-04-10 09:38:22 +0000

    [SQL] Twilight Cryptomancer SAI added (feedback #14087)

--- sql/custom/world/2015_04_07_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_07_world_creature_quest_loots_fix.sql
@@ -983,3 +983,12 @@ INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES (28126, 12513);
 
 /* Cutodian Dieworth now don't start wrong quests */
 DELETE FROM `creature_queststarter` WHERE `quest` IN (10182,10305,10306,10307);
+
+/* Twilight Cryptomancer SAI added */
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 44855;
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=44855 AND `source_type`=0);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(44855, 0, 0, 0, 1, 0, 100, 1, 1, 1, 0, 0, 28, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Twilight Cryptomancer - Remove all aura OOC"),
+(44855, 0, 1, 0, 0, 0, 100, 1, 1, 1, 0, 0, 11, 79849, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Twilight Cryptomancer - Cast Molten Armor"),
+(44855, 0, 2, 0, 0, 0, 100, 0, 9000, 12000, 10000, 12000, 11, 79857, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Twilight Cryptomancer - Cast Blast Wave"),
+(44855, 0, 3, 0, 0, 0, 100, 0, 2500, 3000, 3000, 5000, 11, 79854, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Twilight Cryptomancer - Cast Fireball");

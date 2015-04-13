f596b22e75840b9b1715b08714d7bbe08147d731
Turk Abi <TurkAbi89@gmail.com>
2015-04-13 11:53:47 +0000

    [SQL] Gloom'rel SAI added (feedback #12001)

--- sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
@@ -63,3 +63,11 @@ INSERT INTO `item_loot_template` VALUES
 (67248, 51992, 0, 1, 1, 1, 1),
 (67248, 51997, 0, 1, 1, 1, 1),
 (67248, 51998, 0, 1, 1, 1, 1);
+
+/* Gloom'rel SAI added */
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 9037;
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=9037 AND `source_type`=0);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(9037, 0, 0, 0, 0, 0, 100, 0, 3000, 5000, 6000, 8000, 11, 40504, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Gloom'rel - Cast Cleave"),
+(9037, 0, 1, 0, 9, 0, 100, 0, 0, 4, 20000, 30000, 11, 9080, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Gloom'rel - Hamstring"),
+(9037, 0, 2, 0, 0, 0, 100, 0, 5000, 8000, 10000, 13000, 11, 13737, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Gloom'rel - Cast Mortal Strike");

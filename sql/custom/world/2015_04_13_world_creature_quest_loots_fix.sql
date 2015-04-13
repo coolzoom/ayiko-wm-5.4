03be50bbc8bff71a087f5dea5c6cdb40345b9c7b
Turk Abi <TurkAbi89@gmail.com>
2015-04-13 10:49:22 +0000

    [SQL]  Item: A Crumpled Up Note removed (feedback #11991)

--- sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
@@ -3,3 +3,6 @@ UPDATE `quest_template` SET `Flags` = 8200, `Objectives` = 'Return to Private Ro
 
 /* Quest: Infiltrating Shadowforge City */
 UPDATE `quest_template` SET `Title` = 'Infiltrating Shadowforge City', `Objectives` = 'Speak to Razal''blade.', `Details` = 'We''re fighting a losing battle right now, I''m afraid. The Dark Irons would be difficult to defeat on their own, but their elemental allies have given them new bleeding edge warriors that we simply can''t compete with.$B$BI need you to help us infiltrate their domain and kill as many of their vital leaders as possible. Hopefully that will shock their forces and turn the tide in our favor. Report to Razal''blade for further instruction. He''s stationed beyond this gate and to the right.', `EndText` = 'Return to Razal''blade in Blackrock Depths.' WHERE `Id` = 27582;
+
+/* Item: A Crumpled Up Note removed */
+DELETE FROM `creature_loot_template` WHERE `item`=11446;

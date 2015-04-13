8434f5993fed3e62bceebd02fb93c2be09bbf7f9
Turk Abi <TurkAbi89@gmail.com>
2015-04-13 09:49:37 +0000

    [SQL] Quest: Rocknot's Ale fixed (feedback #11996)

---
+++ sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
@@ -0,0 +1,2 @@
+/* Quest: Rocknot's Ale fixed */
+UPDATE `quest_template` SET `Flags` = 8200, `Objectives` = 'Return to Private Rocknot in Alterac Mountains.', `Details` = 'Oh, ale!  I would kill for some Dark Iron ale!!  Quick, I''m getting sober!  I know this because sobriety blurs my vision... and it''s making you look like the $r I slew last week!' WHERE `Id` = 4295;

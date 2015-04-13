339df3bb78f886fa77bb85d1e84f5d82b0ce8a1e
Turk Abi <TurkAbi89@gmail.com>
2015-04-13 21:29:01 +0000

    [SQL] Lakka SAI fixed (feedback #10653)

--- sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
@@ -160,3 +160,13 @@ INSERT INTO `creature_loot_template` VALUES
 DELETE FROM `areatrigger_teleport` WHERE `id` IN (4459,4612);
 INSERT INTO `areatrigger_teleport` VALUES
 (4612,'The Botanica (Exit)',530,3413.65,1483.32,182.838,2.54432);
+
+/* Lakka SAI fixed */
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=1895600 AND `source_type`=9);
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=18956 AND `source_type`=0);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(18956, 0, 0, 1, 64, 0, 100, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Lakka -  Close Gossip"),
+(18956, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Lakka - Say Text 0 on gossip"),
+(18956, 0, 2, 3, 61, 0, 100, 0, 0, 0, 0, 0, 33, 18956, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "Lakka - Give Quest Credit on gossip"),
+(18956, 0, 3, 4, 61, 0, 100, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 13, 183051, 0, 40, 0, 0, 0, 0, "Lakka - Open Cage on gossip"),
+(18956, 0, 4, 0, 61, 0, 100, 0, 0, 0, 0, 0, 41, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Lakka - Despawn after the gossip");

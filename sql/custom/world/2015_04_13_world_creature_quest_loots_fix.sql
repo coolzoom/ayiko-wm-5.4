394c3af9b4ba1eac2f8643136777a258c25436e5
Turk Abi <TurkAbi89@gmail.com>
2015-04-13 11:59:37 +0000

    [SQL] Mistress Nagmara SAI added (feedback #11997)

--- sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
@@ -71,3 +71,9 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_
 (9037, 0, 0, 0, 0, 0, 100, 0, 3000, 5000, 6000, 8000, 11, 40504, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Gloom'rel - Cast Cleave"),
 (9037, 0, 1, 0, 9, 0, 100, 0, 0, 4, 20000, 30000, 11, 9080, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Gloom'rel - Hamstring"),
 (9037, 0, 2, 0, 0, 0, 100, 0, 5000, 8000, 10000, 13000, 11, 13737, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, "Gloom'rel - Cast Mortal Strike");
+
+/* Mistress Nagmara SAI added */
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 9500;
+DELETE FROM `smart_scripts` WHERE (`entryorguid`=9500 AND `source_type`=0);
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(9500, 0, 0, 0, 62, 0, 100, 0, 2076, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 13, 170571, 0, 200, 0, 0, 0, 0, "Mistress Nagmara - Open the Bar Door on Gossip");

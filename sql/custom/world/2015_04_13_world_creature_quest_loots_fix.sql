a6ec1e3b647d5e458c978332e01f482c8e52f593
Turk Abi <TurkAbi89@gmail.com>
2015-04-16 19:33:45 +0000

    [SQL] Warlord Thresh'jin now drops his QI (feedback #11856)

--- sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
+++ sql/custom/world/2015_04_13_world_creature_quest_loots_fix.sql
@@ -378,3 +378,7 @@ DELETE FROM `smart_scripts` WHERE (`entryorguid`=11897 AND `source_type`=0);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (11897, 0, 0, 0, 8, 0, 100, 0, 85431, 0, 1000, 1000, 49, 0, 0, 0, 0, 0, 0, 21, 200, 0, 0, 0, 0, 0, 0, "Duskwing - Attack when called");
 
+/* Warlord Thresh'jin now drops his QI */
+DELETE FROM `creature_loot_template` WHERE (`entry`=10822) AND (`item`=61315);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+(10822, 61315, -100, 1, 0, 1, 1);

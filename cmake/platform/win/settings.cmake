672fcb1b54070b48adcf4495ac4956c3ff900cdb
zzhelyazkov <dirtyleet@gmail.com>
2015-04-22 03:10:16 +0300

    Fixup compile

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -1,4 +1,8 @@
 # Package overloads
+set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/acelite)
+set(ACE_LIBRARY "ace")
+set(ZEROMQ_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/zmq)
+set(ZEROMQ_LIBRARY ${CMAKE_SOURCE_DIR}/dep/zmq/zmq.lib)
 set(BZIP2_LIBRARIES "bzip2")
 set(ZLIB_LIBRARIES "zlib")
 set(ZLIB_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/dep/zlib)
